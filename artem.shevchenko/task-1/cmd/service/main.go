641e47143b6913756d130f6839d8c300cca0484e
slendycs <slendycs@yandex.ru>
2025-09-17 23:11:49 +0300

    [TASK-1] Data entry prompts have been removed

--- artem.shevchenko/task-1/cmd/service/main.go
+++ artem.shevchenko/task-1/cmd/service/main.go
@@ -8,7 +8,6 @@ func main() {
 	var operator string
 
 	// Read the first operand
-	fmt.Println("Enter the first number: ")
 	_, err := fmt.Scan(&num1)
 	if err != nil {
 		fmt.Println("Invalid first operand")
@@ -16,7 +15,6 @@ func main() {
 	}
 
 	// Read the second operand
-	fmt.Println("Enter the second number:")
 	_, err = fmt.Scan(&num2)
 	if err != nil {
 		fmt.Println("Invalid second operand")
@@ -24,7 +22,6 @@ func main() {
 	}
 
 	// Read the operation sign
-	fmt.Println("Enter operator: ")
 	_, err = fmt.Scan(&operator)
 	if err != nil {
 		return
