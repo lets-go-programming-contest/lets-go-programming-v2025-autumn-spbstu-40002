16652cddc070cb2ea85922306c48e54ad7a7a890
Mashin Andrew <mashin.ad@edu.spbstu.ru>
2025-09-17 17:52:57 +0300

    [TASK-1] Initial commit

---
+++ mashin.andrew/task-1/cmd/service/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	var number1 int
+	_, err1 := fmt.Scan(&number1)
+
+	if err1 != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+
+	var number2 int
+	_, err2 := fmt.Scan(&number2)
+
+	if err2 != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	var operator string
+	fmt.Scan(&operator)
+
+	switch operator {
+	case "+":
+		fmt.Print(number1 + number2)
+	case "-":
+		fmt.Print(number1 - number2)
+	case "/":
+		if number2 != 0 {
+			fmt.Print(number1 / number2)
+		} else {
+			fmt.Print("Division by zero")
+		}
+	case "*":
+		fmt.Print(number1 * number2)
+	default:
+		fmt.Print("Invalid operation")
+	}
+}
