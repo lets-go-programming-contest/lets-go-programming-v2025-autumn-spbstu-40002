b4414454d89ff4d80a5ab211e1204cf28bc7fd4b
slendycs <slendycs@yandex.ru>
2025-09-17 22:58:23 +0300

    [TASK-1] Initial commit

---
+++ artem.shevchenko/task-1/cmd/service/main.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num1 int
+	var num2 int
+	var operator string
+
+	// Read the first operand
+	fmt.Println("Enter the first number: ")
+	_, err := fmt.Scan(&num1)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+
+	// Read the second operand
+	fmt.Println("Enter the second number:")
+	_, err = fmt.Scan(&num2)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	// Read the operation sign
+	fmt.Println("Enter operator: ")
+	fmt.Scan(&operator)
+
+	// Сalculate the expression depending on the operator
+	switch operator {
+	case "+":
+		fmt.Println(num1 + num2)
+
+	case "-":
+		fmt.Println(num1 - num2)
+
+	case "*":
+		fmt.Println(num1 * num2)
+
+	case "/":
+		if num2 == 0 {
+			fmt.Println("Division by zero")
+			return
+		}
+		fmt.Println(num1 / num2)
+
+	default:
+		fmt.Println("Invalid operation")
+		return
+	}
+}
