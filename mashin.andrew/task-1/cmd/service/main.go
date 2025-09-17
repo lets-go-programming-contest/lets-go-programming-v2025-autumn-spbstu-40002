ba1c0a8c4d0849d87b79ca0a1c8c1136454157f4
Andrew <97516045+Exam-Play@users.noreply.github.com>
2025-09-17 19:18:12 +0300

    [TASK-1] Initial commit

--- mashin.andrew/task-1/cmd/service/main.go
+++ mashin.andrew/task-1/cmd/service/main.go
@@ -26,18 +26,18 @@ func main() {
 
 	switch operator {
 	case "+":
-		fmt.Print(number1 + number2)
+		fmt.Print(number1 + number2, "\n")
 	case "-":
-		fmt.Print(number1 - number2)
+		fmt.Print(number1 - number2, "\n")
 	case "/":
 		if number2 != 0 {
-			fmt.Print(number1 / number2)
+			fmt.Print(number1 / number2, "\n")
 		} else {
 			fmt.Printf("Division by zero\n")
 		}
 	case "*":
-		fmt.Print(number1 * number2)
+		fmt.Print(number1 * number2, "\n")
 	default:
-		fmt.Print("Invalid operation")
+		fmt.Printf("Invalid operation\n")
 	}
 }
