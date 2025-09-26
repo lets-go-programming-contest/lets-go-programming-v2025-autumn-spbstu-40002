81ccc913cb36a3342ebf2e0b1bb3a33d814554b9
Leon-Maryankov <mariannnkovleonid@gmail.com>
2025-09-26 15:45:51 +0300

    [TASK-1] Changed error names and corrected type

--- leonid.maryankov/task-1/cmd/service/main.go
+++ leonid.maryankov/task-1/cmd/service/main.go
@@ -4,23 +4,23 @@ import "fmt"
 
 func main() {
 	var (
-		argOne float64
-		argTwo float64
+		argOne int
+		argTwo int
 		symbol string
 	)
 
-	_, errOne := fmt.Scan(&argOne)
-	if errOne != nil {
+	_, err := fmt.Scan(&argOne)
+	if err != nil {
 		fmt.Println("Invalid first operand")
 		return
 	}
-	_, errTwo := fmt.Scan(&argTwo)
-	if errTwo != nil {
+	_, err = fmt.Scan(&argTwo)
+	if err != nil {
 		fmt.Println("Invalid second operand")
 		return
 	}
-	_, errSym := fmt.Scan(&symbol)
-	if errSym != nil {
+	_, err = fmt.Scan(&symbol)
+	if err != nil {
 		fmt.Println("Invalid operation")
 		return
 	}
