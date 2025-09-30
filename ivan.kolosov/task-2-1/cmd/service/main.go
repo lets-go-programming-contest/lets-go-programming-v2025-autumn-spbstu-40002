7eca8f0f18c47cc54dab518fbdbaf9fc10040c64
InsomniaDemon <chelovefick@gmail.com>
2025-09-30 20:27:07 +0300

    [TASK-2-1] Initial commit

---
+++ ivan.kolosov/task-2-1/cmd/service/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	MIN = 15
+	MAX = 30
+)
+
+func main() {
+	var amountOfDepartments int
+	_, err := fmt.Scan(&amountOfDepartments)
+	if err != nil || amountOfDepartments < 1 || amountOfDepartments > 1000 {
+		fmt.Println("Incorrect amount of departments")
+		return
+	}
+
+	var sign string
+	var leftBorder int
+	var rightBorder int
+	var newBorder int
+	var amountOfEmployees int
+
+	for range amountOfDepartments {
+		leftBorder = MIN
+		rightBorder = MAX
+		_, err = fmt.Scan(&amountOfEmployees)
+		if err != nil || amountOfEmployees < 1 || amountOfEmployees > 1000 {
+			fmt.Println("Incorrect amount of employees")
+			return
+		}
+		for range amountOfEmployees {
+			if leftBorder == -1 {
+				fmt.Println(leftBorder)
+				continue
+			}
+
+			_, err = fmt.Scan(&sign)
+			if err != nil {
+				return
+			}
+
+			_, err = fmt.Scan(&newBorder)
+			if err != nil || newBorder > 30 || newBorder < 15 {
+				fmt.Println("Incorrect border")
+				return
+			}
+
+			switch sign {
+			case "<=":
+				if newBorder < leftBorder {
+					leftBorder = -1
+				} else if newBorder < rightBorder {
+					rightBorder = newBorder
+				}
+			case ">=":
+				if newBorder > rightBorder {
+					leftBorder = -1
+				} else if newBorder > leftBorder {
+					leftBorder = newBorder
+				}
+			default:
+				fmt.Println("Incorrect sign")
+				return
+			}
+
+			fmt.Println(leftBorder)
+		}
+	}
+}
