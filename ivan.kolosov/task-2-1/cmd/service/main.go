66a05392617607ca1789da99cd845f5960feb4bd
InsomniaDemon <chelovefick@gmail.com>
2025-09-30 20:59:33 +0300

    [TASK-2-1] fiting with lint

--- ivan.kolosov/task-2-1/cmd/service/main.go
+++ ivan.kolosov/task-2-1/cmd/service/main.go
@@ -14,37 +14,44 @@ func main() {
 	_, err := fmt.Scan(&amountOfDepartments)
 	if err != nil || amountOfDepartments < 1 || amountOfDepartments > 1000 {
 		fmt.Println("Incorrect amount of departments")
+		
 		return
 	}
 
 	var sign string
-	var leftBorder int
-	var rightBorder int
-	var newBorder int
+	leftBorder := MIN
+	rightBorder := MAX
+	newBorder := 0
 	var amountOfEmployees int
 
 	for range amountOfDepartments {
 		leftBorder = MIN
 		rightBorder = MAX
+		
 		_, err = fmt.Scan(&amountOfEmployees)
 		if err != nil || amountOfEmployees < 1 || amountOfEmployees > 1000 {
 			fmt.Println("Incorrect amount of employees")
+			
 			return
 		}
+		
 		for range amountOfEmployees {
 			if leftBorder == -1 {
 				fmt.Println(leftBorder)
+				
 				continue
 			}
 
 			_, err = fmt.Scan(&sign)
 			if err != nil {
+				
 				return
 			}
 
 			_, err = fmt.Scan(&newBorder)
 			if err != nil || newBorder > 30 || newBorder < 15 {
 				fmt.Println("Incorrect border")
+				
 				return
 			}
 
@@ -63,6 +70,7 @@ func main() {
 				}
 			default:
 				fmt.Println("Incorrect sign")
+				
 				return
 			}
 
