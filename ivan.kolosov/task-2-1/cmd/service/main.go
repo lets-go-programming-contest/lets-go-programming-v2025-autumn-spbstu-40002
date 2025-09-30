f6a04e6ccc2852165953a980b8a721bb7c866d7b
InsomniaDemon <chelovefick@gmail.com>
2025-10-01 00:30:47 +0300

    [TASK-2-1] Finally fixing test and some liner issues again (that's definitely not the end)

--- ivan.kolosov/task-2-1/cmd/service/main.go
+++ ivan.kolosov/task-2-1/cmd/service/main.go
@@ -1,52 +1,62 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
 const (
-	TEMPERATURE_MIN = 15
-	TEMPERATURE_MAX = 30
-	DEP_EMP_MAX     = 1000
-	DEP_EMP_MIN     = 1
+	TemperatureMin = 15
+	TemperatureMax = 30
+	DepEmpMin      = 1
+	DepEmpMax      = 1000
+)
+
+var (
+	ErrorIncorrectSign        = errors.New("incorrect sign")
+	ErrorIncorrectBorder      = errors.New("incorrect border")
+	ErrorIncorrectDepartments = errors.New("incorrect amount of departments")
+	ErrorIncorrectEmployees   = errors.New("incorrect amount of employees")
 )
 
 func loopForSpecificDepartment(leftBorder int, rightBorder int, amountOfEmployees int) error {
 	var newBorder int
+
 	var sign string
 
 	for range amountOfEmployees {
+
+		_, err := fmt.Scan(&sign)
+		if err != nil {
+			return ErrorIncorrectSign
+		}
+
+		_, err = fmt.Scan(&newBorder)
+		if err != nil || newBorder > TemperatureMax || newBorder < TemperatureMin {
+			return ErrorIncorrectBorder
+		}
+
 		if leftBorder == -1 {
 			fmt.Println(leftBorder)
 
 			continue
 		}
 
-		_, err := fmt.Scan(&sign)
-		if err != nil {
-			return fmt.Errorf("incorrect sign")
-		}
-
-		_, err = fmt.Scan(&newBorder)
-		if err != nil || newBorder > TEMPERATURE_MAX || newBorder < TEMPERATURE_MIN {
-			return fmt.Errorf("incorrect border")
-		}
-
 		switch sign {
 		case "<=":
-			if newBorder < leftBorder {
-				leftBorder = -1
-			} else if newBorder < rightBorder {
+			if newBorder < rightBorder {
 				rightBorder = newBorder
 			}
 		case ">=":
-			if newBorder > rightBorder {
-				leftBorder = -1
-			} else if newBorder > leftBorder {
+			if newBorder > leftBorder {
 				leftBorder = newBorder
 			}
 		default:
-			return fmt.Errorf("incorrect sign")
+			return ErrorIncorrectSign
+		}
+
+		if leftBorder > rightBorder {
+			leftBorder = -1
 		}
 
 		fmt.Println(leftBorder)
@@ -58,8 +68,8 @@ func main() {
 	var amountOfDepartments int
 	_, err := fmt.Scan(&amountOfDepartments)
 
-	if err != nil || amountOfDepartments < DEP_EMP_MIN || amountOfDepartments > DEP_EMP_MAX {
-		fmt.Println("Error: incorrect amount of departments")
+	if err != nil || amountOfDepartments < DepEmpMin || amountOfDepartments > DepEmpMax {
+		fmt.Println("Error:", ErrorIncorrectDepartments)
 
 		return
 	}
@@ -68,13 +78,13 @@ func main() {
 
 	for range amountOfDepartments {
 		_, err = fmt.Scan(&amountOfEmployees)
-		if err != nil || amountOfEmployees < DEP_EMP_MIN || amountOfEmployees > DEP_EMP_MAX {
-			fmt.Println("Error: incorrect amount of employees")
+		if err != nil || amountOfEmployees < DepEmpMin || amountOfEmployees > DepEmpMax {
+			fmt.Println("Error:", ErrorIncorrectEmployees)
 
 			return
 		}
 
-		err = loopForSpecificDepartment(TEMPERATURE_MIN, TEMPERATURE_MAX, amountOfEmployees)
+		err = loopForSpecificDepartment(TemperatureMin, TemperatureMax, amountOfEmployees)
 		if err != nil {
 			fmt.Println("Error:", err)
 
