46cf0f39ad02100d7647fba6cbc04d8ed8d59f2b
eg0sha-0 <geneziz777@bk.ru>
2025-09-30 22:46:10 +0300

    [TASK-2-1] divided into functions

--- egor.kovalchuk/task-2-1/cmd/service/main.go
+++ egor.kovalchuk/task-2-1/cmd/service/main.go
@@ -9,71 +9,65 @@ const (
 
 func main() {
 	var departments int
-	if _, err := fmt.Scan(&departments); err != nil {
-		return
-	}
-	if departments < 0 {
+	if _, err := fmt.Scan(&departments); err != nil || departments < 0 {
 		return
 	}
 
 	depts := make([]struct{}, departments)
 	for range depts {
-		low := defaultLow
-		high := defaultHigh
-
-		var employeeCount int
-		if _, err := fmt.Scan(&employeeCount); err != nil {
+		if ok := processDepartment(); !ok {
 			return
-		}
-		if employeeCount < 0 {
-			return
-		}
-
-		contradiction := false
-
-		emps := make([]struct{}, employeeCount)
-		for range emps {
-			operator := ""
-			temperature := 0
-
-			_, err := fmt.Scan(&operator, &temperature)
-			if err != nil {
-				return
-			}
-
-			if contradiction {
-				fmt.Println(-1)
-
-				continue
-			}
-
-			if operator != ">=" && operator != "<=" {
-				contradiction = true
-				fmt.Println(-1)
-
-				continue
-			}
-
-			switch operator {
-			case ">=":
-				if temperature > low {
-					low = temperature
-				}
-			case "<=":
-				if temperature < high {
-					high = temperature
-				}
-			}
-
-			if low > high {
-				contradiction = true
-
-				fmt.Println(-1)
-
-				continue
-			}
-
-			fmt.Println(low)
 		}
 	}
+}
+
+func processDepartment() bool {
+	low, high := defaultLow, defaultHigh
+
+	var employeeCount int
+	if _, err := fmt.Scan(&employeeCount); err != nil || employeeCount < 0 {
+		return false
+	}
+
+	contradiction := false
+	emps := make([]struct{}, employeeCount)
+	for range emps {
+		var operator string
+		var temperature int
+		if _, err := fmt.Scan(&operator, &temperature); err != nil {
+			return false
+		}
+
+		if contradiction {
+			fmt.Println(-1)
+			continue
+		}
+
+		if operator != ">=" && operator != "<=" {
+			contradiction = true
+			fmt.Println(-1)
+			continue
+		}
+
+		switch operator {
+		case ">=":
+			if temperature > low {
+				low = temperature
+			}
+		case "<=":
+			if temperature < high {
+				high = temperature
+			}
+		}
+
+		if low > high {
+			contradiction = true
+			fmt.Println(-1)
+			continue
+		}
+
+		fmt.Println(low)
+	}
+
+	return true
 }
