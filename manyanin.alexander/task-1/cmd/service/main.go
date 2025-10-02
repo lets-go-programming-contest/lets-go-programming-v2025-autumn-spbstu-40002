d920bf74e9393e043a46414a1c1a7b6de231cd87
Alexander Manyanin <almen69@yandex.ru>
2025-10-02 22:12:44 +0300

    [TASK-1] Var names changed

--- manyanin.alexander/task-1/cmd/service/main.go
+++ manyanin.alexander/task-1/cmd/service/main.go
@@ -3,18 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	var first_operand int
-	var second_operand int
+	var firstОperand int
+	var secondOperand int
 
 	var operator string
 
-	_, err := fmt.Scan(&first_operand)
+	_, err := fmt.Scan(&firstОperand)
 	if err != nil {
 		fmt.Println("Invalid first operand")
 		return
 	}
 
-	_, err = fmt.Scan(&second_operand)
+	_, err = fmt.Scan(&secondOperand)
 	if err != nil {
 		fmt.Println("Invalid first operand")
 		return
@@ -27,17 +27,17 @@ func main() {
 
 	switch operator {
 	case "*":
-		fmt.Println(first_operand * second_operand)
+		fmt.Println(firstОperand * secondOperand)
 	case "/":
-		if second_operand == 0 {
+		if secondOperand == 0 {
 			fmt.Println("Division by zero")
 			return
 		}
-		fmt.Println(first_operand / second_operand)
+		fmt.Println(firstОperand / secondOperand)
 	case "-":
-		fmt.Println(first_operand - second_operand)
+		fmt.Println(firstОperand - secondOperand)
 	case "+":
-		fmt.Println(first_operand + second_operand)
+		fmt.Println(firstОperand + secondOperand)
 
 	default:
 		fmt.Println("Invalid operation")
