c5c9aa3e183dda9c387ad19cd36402c1dbbf5527
slendycs <slendycs@yandex.ru>
2025-10-02 21:13:32 +0300

    [TASK-2-1] initial commit

---
+++ artem.shevchenko/task-2-1/cmd/service/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+	"lab-2-1/internal/errors"
+)
+
+func fillTemperatureTable(temperatures map[int]int, operator string, temp int) error {
+	// determine acceptable temperature ranges for employees
+	switch operator {
+	case ">=":
+		for currentTemp := temp; currentTemp <= 30; currentTemp++ {
+			temperatures[currentTemp] += 1
+		}
+	case "<=":
+		for currentTemp := temp; currentTemp >= 15; currentTemp-- {
+			temperatures[currentTemp] += 1
+		}
+	default:
+		return errors.ErrIncorrectOperator
+	}
+	return nil
+}
+
+func getAcceptableTemp(temperatures map[int]int, employeeCount int) int {
+	acceptableTemperatures := make([]int, 0)
+
+	// determine a list of temperatures suitable for each employee
+	for temp := 15; temp <= 30; temp++ {
+		if temperatures[temp] == employeeCount {
+			acceptableTemperatures = append(acceptableTemperatures, temp)
+		}
+	}
+
+	// find the minimum temperature
+	if len(acceptableTemperatures) != 0 {
+		return slices.Min(acceptableTemperatures)
+	}
+
+	return -1
+}
+
+
+func main() {
+	var departmentCount, employeeCount, temp int
+	var operator string
+
+	// get the number of departments
+	_, err := fmt.Scan(&departmentCount)
+	if err != nil || !(1 <= departmentCount && departmentCount <= 1000) {
+		fmt.Println(errors.ErrIncorrectDepartmentsCount)
+		return
+	}
+
+	for departmentId := 0; departmentId < departmentCount; departmentId++ {
+
+		// get the number of employees in the department
+		_, err = fmt.Scan(&employeeCount)
+		if err != nil || !(1 <= employeeCount && employeeCount <= 1000) {
+			fmt.Println(errors.ErrIncorrectEmployeeCount)
+	 		return
+		}
+
+		// initialize the map of temperatures
+		temperatures := make(map[int]int)
+
+		for employee := 1; employee <= employeeCount; employee++ {
+			
+			// get the permissible temperature
+			_, err = fmt.Scan(&operator, &temp)
+			if err != nil || !(15 <= temp && temp <= 30) {
+				fmt.Println(errors.ErrIncorrectTemperature)
+				return
+			}
+
+			// filling out the temperature table
+			err = fillTemperatureTable(temperatures, operator, temp)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			// derive the permissible temperature
+			fmt.Println(getAcceptableTemp(temperatures, employee))
+		}
+	}
+}
