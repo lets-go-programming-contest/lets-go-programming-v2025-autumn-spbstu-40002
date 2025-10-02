520cc28fb3b1074b96a526b8b9ab125f9c0c7f56
eg0sha-0 <geneziz777@bk.ru>
2025-10-02 15:47:32 +0300

    [TASK-2-2] Add validation and put it in a separate package

--- egor.kovalchuk/task-2-2/cmd/service/main.go
+++ egor.kovalchuk/task-2-2/cmd/service/main.go
@@ -3,43 +3,16 @@ package main
 import (
 	"container/heap"
 	"fmt"
+
+	"github.com/eg0sha-0/task-2-2/heaputils"
 )
-
-type IntHeap []int
-
-func (ih *IntHeap) Len() int {
-	return len(*ih)
-}
-
-func (ih *IntHeap) Less(i, j int) bool {
-	return (*ih)[i] < (*ih)[j] // min-heap
-}
-
-func (ih *IntHeap) Swap(i, j int) {
-	(*ih)[i], (*ih)[j] = (*ih)[j], (*ih)[i]
-}
-
-func (ih *IntHeap) Push(x interface{}) {
-	v, ok := x.(int)
-	if !ok {
-		panic("IntHeap: Push expects int")
-	}
-
-	*ih = append(*ih, v)
-}
-
-func (ih *IntHeap) Pop() interface{} {
-	old := *ih
-	n := len(old)
-	x := old[n-1]
-	*ih = old[:n-1]
-
-	return x
-}
 
 func main() {
 	var total int
 	if _, err := fmt.Scan(&total); err != nil {
+		return
+	}
+	if total <= 0 {
 		return
 	}
 
@@ -54,12 +27,11 @@ func main() {
 	if _, err := fmt.Scan(&kth); err != nil {
 		return
 	}
-
 	if kth <= 0 || kth > total {
 		return
 	}
 
-	heapInst := &IntHeap{}
+	heapInst := &heaputils.IntHeap{}
 	heap.Init(heapInst)
 
 	for i := range arr[:kth] {
