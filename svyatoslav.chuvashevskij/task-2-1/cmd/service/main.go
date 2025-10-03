cf8e3ba0bc67d1e68e15cb4fccb08d2ebece3dd3
Chuvashevskij Svyatoslav <chuvashevskij2006@gmail.com>
2025-10-03 20:39:48 +0300

    [TASK-2-1] linter errors have been fixed

--- svyatoslav.chuvashevskij/task-2-1/cmd/service/main.go
+++ svyatoslav.chuvashevskij/task-2-1/cmd/service/main.go
@@ -6,32 +6,43 @@ import (
 
 func main() {
 	const maxTemperature = 30
+
 	const minTemperature = 15
 
 	numberOfDepartments := 0
+
 	numberOfEmployees := 0
 
 	_, err := fmt.Scan(&numberOfDepartments)
 	if err != nil {
+
 		fmt.Println("Error:", err)
+
 		return
 	}
 
-	for i := 0; i < numberOfDepartments; i++ {
+	for range numberOfDepartments {
+
 		_, err = fmt.Scan(&numberOfEmployees)
 		if err != nil {
+
 			fmt.Println("Error:", err)
+
 			return
 		}
 
 		conditions := [2]int{minTemperature, maxTemperature}
-		for j := 0; j < numberOfEmployees; j++ {
+		for range numberOfEmployees {
+
 			var option string
+
 			var temperature int
 
 			_, err = fmt.Scan(&option, &temperature)
 			if err != nil {
+
 				fmt.Println("Error:", err)
+
 				return
 			}
 
@@ -46,6 +57,7 @@ func main() {
 				}
 			default:
 				fmt.Println("Error: invalid option")
+
 				return
 			}
 
