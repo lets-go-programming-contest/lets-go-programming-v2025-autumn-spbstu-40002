f768f207e31d75fcc56b579f40a457fd60fe3fe1
Karmanova Alyona <karaliona2006@gmail.com>
2025-10-03 12:00:13 +0300

    [TASK-2-1] Update main.go with first changes for lint

--- alyona.karmanova/task-2-1/cmd/service/main.go
+++ alyona.karmanova/task-2-1/cmd/service/main.go
@@ -6,7 +6,6 @@ import (
 
 func isCorrectInputTemperature(sign string, temp int) bool {
 	return len(sign) == 2 && (sign[0] == '>' || sign[0] == '<') && sign[1] == '=' && (15 <= temp && temp <= 30)
-
 }
 
 func isCorrectInputCnt(c int) bool {
@@ -20,13 +19,16 @@ func applyLowerBound(currentMax *int, currentMin *int, newBoard int, currentTemp
 
 	if newBoard > *currentMin {
 		*currentMin = newBoard
+
 		if currentTemp < newBoard {
 			currentTemp = newBoard
 		}
 	}
+
 	if currentTemp == -1 {
 		currentTemp = newBoard
 	}
+
 	return currentTemp
 }
 
@@ -37,38 +39,45 @@ func applyUpperBound(currentMax *int, currentMin *int, desiredTemp int, currentT
 
 	if desiredTemp < *currentMax {
 		*currentMax = desiredTemp
+
 		if currentTemp > desiredTemp {
 			currentTemp = desiredTemp
 		}
 	}
+
 	if currentTemp == -1 {
 		currentTemp = desiredTemp
 	}
-	return currentTemp
 
+	return currentTemp
 }
+
 func main() {
 	var countDepartment, countPeople int
 	var sign string
 	_, err := fmt.Scan(&countDepartment)
+
 	if err != nil || !isCorrectInputCnt(countDepartment) {
 		return
 	}
 
-	for i := 0; i < countDepartment; i++ {
+	for range countDepartment {
 		_, err = fmt.Scan(&countPeople)
+
 		if err != nil || !isCorrectInputCnt(countPeople) {
 			return
 		}
-		var tempMin, tempMax int = 14, 31
-		var curTemp int = -1
+		tempMin, tempMax, curTemp := 14, 31, -1
 		var newBoard int
-		for j := 0; j < countPeople; j++ {
+
+		for range countPeople {
 			_, err = fmt.Scan(&sign, &newBoard)
+
 			if err != nil || !isCorrectInputTemperature(sign, newBoard) {
 				return
 			}
 			isLookGreater := sign[0] == '>'
+
 			if isLookGreater {
 				curTemp = applyLowerBound(&tempMax, &tempMin, newBoard, curTemp)
 			} else {
