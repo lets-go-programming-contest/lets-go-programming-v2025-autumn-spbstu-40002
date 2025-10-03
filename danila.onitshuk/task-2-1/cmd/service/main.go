a330f9b955161f522166a5a8ba556ca973b63d2d
danila-clown <daphetad@gmail.com>
2025-10-03 03:45:23 +0000

    [TASK-2-1] add main.go

---
+++ danila.onitshuk/task-2-1/cmd/service/main.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+func main() {
+	var departmentsCount int
+	_, err := fmt.Scan(&departmentsCount)
+	if err != nil {
+		return
+	}
+
+	for range departmentsCount {
+		minTemperature := 15
+		maxTemperature := 30
+		var employeesCount int
+		_, err := fmt.Scan(&employeesCount)
+		if err != nil {
+			return
+		}
+
+		for range employeesCount {
+			var (
+				sign        string
+				temperature int
+			)
+			if _, err := fmt.Scan(&sign, &temperature); err != nil {
+				return
+			}
+
+			switch sign {
+			case ">=":
+				if temperature > minTemperature {
+					minTemperature = temperature
+				}
+			case "<=":
+				if temperature < maxTemperature {
+					maxTemperature = temperature
+				}
+			}
+
+			if minTemperature <= maxTemperature && minTemperature >= 15 && maxTemperature <= 30 {
+				fmt.Println(minTemperature)
+			} else {
+				fmt.Println(-1)
+			}
+		}
+	}
+}
