4b70a5a4be1993399cfb8713742bb722141cadab
danila-clown <daphetad@gmail.com>
2025-10-03 04:11:20 +0000

    [TASK-2-1] reducing the cyclic function

--- danila.onitshuk/task-2-1/cmd/service/main.go
+++ danila.onitshuk/task-2-1/cmd/service/main.go
@@ -2,6 +2,47 @@ package main
 
 import "fmt"
 
+func department() {
+	minTemperature := 15
+	maxTemperature := 30
+
+	var employeesCount int
+
+	_, err := fmt.Scan(&employeesCount)
+	if err != nil {
+		return
+	}
+
+	for range employeesCount {
+		var (
+			sign        string
+			temperature int
+		)
+
+		_, err := fmt.Scan(&sign, &temperature)
+		if err != nil {
+			return
+		}
+
+		switch sign {
+		case ">=":
+			if temperature > minTemperature {
+				minTemperature = temperature
+			}
+		case "<=":
+			if temperature < maxTemperature {
+				maxTemperature = temperature
+			}
+		}
+
+		if minTemperature <= maxTemperature && minTemperature >= 15 && maxTemperature <= 30 {
+			fmt.Println(minTemperature)
+		} else {
+			fmt.Println(-1)
+		}
+	}
+}
+
 func main() {
 	var departmentsCount int
 
@@ -11,43 +52,6 @@ func main() {
 	}
 
 	for range departmentsCount {
-		minTemperature := 15
-		maxTemperature := 30
-
-		var employeesCount int
-
-		_, err := fmt.Scan(&employeesCount)
-		if err != nil {
-			return
-		}
-
-		for range employeesCount {
-			var (
-				sign        string
-				temperature int
-			)
-
-			_, err := fmt.Scan(&sign, &temperature)
-			if err != nil {
-				return
-			}
-
-			switch sign {
-			case ">=":
-				if temperature > minTemperature {
-					minTemperature = temperature
-				}
-			case "<=":
-				if temperature < maxTemperature {
-					maxTemperature = temperature
-				}
-			}
-
-			if minTemperature <= maxTemperature && minTemperature >= 15 && maxTemperature <= 30 {
-				fmt.Println(minTemperature)
-			} else {
-				fmt.Println(-1)
-			}
-		}
+		department()
 	}
 }
