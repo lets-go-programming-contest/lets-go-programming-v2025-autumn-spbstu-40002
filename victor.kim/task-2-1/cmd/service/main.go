161240e2798a82fe64548deb5297189dbb6e3c3a
л1ьмщ <kimvo1511@gmail.com>
2025-10-03 20:28:03 +0300

    [TASK-2-1] Initial Commit attempt 4]5

--- victor.kim/task-2-1/cmd/service/main.go
+++ victor.kim/task-2-1/cmd/service/main.go
@@ -14,84 +14,101 @@ var (
 
 func parseBorder(border string) (string, int, error) {
 	fields := strings.Fields(border)
-	if len(fields) != 2 {
-		return "", 0, ErrInvalidTemperature
-	}
+
 	if fields[0] != "<=" && fields[0] != ">=" {
 		return "", 0, ErrInvalidOperator
 	}
+
 	temp, err := strconv.Atoi(fields[1])
 	if err != nil {
 		return "", 0, ErrInvalidTemperature
 	}
-	if temp < 15 || temp > 30 {
+
+	if temp > 30 || temp < 15 {
 		return "", 0, ErrInvalidTemperature
 	}
+
 	return fields[0], temp, nil
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
 }
 
 func processEmployees(employees int) {
 	minTemp := 15
 	maxTemp := 30
 	impossibleCondition := false
-	for i := 0; i < employees; i++ {
-		var oper string
-		var temp string
+
+	for range employees {
+		var (
+			oper string
+			temp string
+		)
+
 		_, err := fmt.Scan(&oper, &temp)
 		if err != nil {
+			fmt.Println("Error reading operator and temperature:", err)
 			fmt.Println(-1)
+
 			impossibleCondition = true
+
 			continue
 		}
-		operation, temperature, err := parseBorder(fmt.Sprintf("%s %s", oper, temp))
+
+		tempBorder := fmt.Sprintf("%s %s", oper, temp)
+
+		operation, temperature, err := parseBorder(tempBorder)
 		if err != nil {
+			fmt.Println("Error has occurred:", err)
 			fmt.Println(-1)
+
 			impossibleCondition = true
+
 			continue
 		}
+
+		if impossibleCondition {
+			fmt.Println(-1)
+
+			continue
+		}
+
 		switch operation {
 		case ">=":
 			minTemp = max(minTemp, temperature)
 		case "<=":
 			maxTemp = min(maxTemp, temperature)
 		}
+
 		if minTemp > maxTemp {
 			fmt.Println(-1)
+
 			impossibleCondition = true
+
 			continue
 		}
+
 		fmt.Println(minTemp)
 	}
 }
 
 func main() {
 	var departments int
+
 	_, err := fmt.Scan(&departments)
 	if err != nil {
 		fmt.Println("Error reading number of departments:", err)
+
 		return
 	}
-	for i := 0; i < departments; i++ {
+
+	for range departments {
 		var employees int
+
 		_, err := fmt.Scan(&employees)
 		if err != nil {
 			fmt.Println("Error reading number of employees:", err)
+
 			return
 		}
+
 		processEmployees(employees)
 	}
 }
