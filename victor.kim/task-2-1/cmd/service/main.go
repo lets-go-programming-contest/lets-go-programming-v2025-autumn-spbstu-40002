26410db27bcc9b1b48e7cdff179467b0ebd7353a
л1ьмщ <kimvo1511@gmail.com>
2025-10-04 00:42:55 +0300

    [TASK-2-1] Initial commit for task2-1 attempt 2

--- victor.kim/task-2-1/cmd/service/main.go
+++ victor.kim/task-2-1/cmd/service/main.go
@@ -12,11 +12,9 @@ const (
 
 var errFormat = errors.New("invalid temperature format")
 
-func adjustTemperature(currentLow int, currentHigh int) (int, int, error) {
-	var (
-		operator string
-		value    int
-	)
+func adjustTemperature(low int, high int) (int, int, error) {
+	var operator string
+	var value int
 
 	_, err := fmt.Scan(&operator, &value)
 	if err != nil || value < minTemperature || value > maxTemperature {
@@ -25,49 +23,52 @@ func adjustTemperature(currentLow int, currentHigh int) (int, int, error) {
 
 	switch operator {
 	case ">=":
-		if value > currentHigh {
+		if value > high {
 			return -1, -1, nil
 		}
-
-		if value > currentLow {
-			currentLow = value
+		if value > low {
+			low = value
 		}
 	case "<=":
-		if value < currentLow {
+		if value < low {
 			return -1, -1, nil
 		}
-
-		if value < currentHigh {
-			currentHigh = value
+		if value < high {
+			high = value
 		}
 	default:
 		return 0, 0, errFormat
 	}
 
-	return currentLow, currentHigh, nil
+	return low, high, nil
 }
 
 func main() {
-	var departments uint16
-	if _, err := fmt.Scan(&departments); err != nil || departments == 0 || departments > 1000 {
+	var departments int
+	_, err := fmt.Scan(&departments)
+	if err != nil || departments <= 0 || departments > 1000 {
 		fmt.Println("invalid number of departments")
+
 		return
 	}
 
-	for d := uint16(0); d < departments; d++ {
-		var employees uint16
-		if _, err := fmt.Scan(&employees); err != nil || employees == 0 || employees > 1000 {
+	for d := 0; d < departments; d++ {
+		var employees int
+		_, err := fmt.Scan(&employees)
+		if err != nil || employees <= 0 || employees > 1000 {
 			fmt.Println("invalid number of employees")
+
 			return
 		}
 
 		currentLow := minTemperature
 		currentHigh := maxTemperature
 
-		for e := uint16(0); e < employees; e++ {
-			currentLow, currentHigh, err := adjustTemperature(currentLow, currentHigh)
+		for e := 0; e < employees; e++ {
+			currentLow, currentHigh, err = adjustTemperature(currentLow, currentHigh)
 			if err != nil {
 				fmt.Println(err)
+
 				return
 			}
 
