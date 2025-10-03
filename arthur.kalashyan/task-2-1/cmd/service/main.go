4a80776da8cf5d8297f368328331e2321260484d
Expeline <kalashyan.artur@list.ru>
2025-10-03 17:15:53 +0300

    [TASK-1] Created constants, additional function, simplified field names

--- arthur.kalashyan/task-2-1/cmd/service/main.go
+++ arthur.kalashyan/task-2-1/cmd/service/main.go
@@ -1,39 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+const (
+	MinTemp        = 15
+	MaxTemp        = 30
+	MinValidNumber = 1
+	MaxValidNumber = 1000
+)
+
+func findOptimal(minTemperature, maxTemperature int, operator string, requestedTemperature int) (int, int) {
+	if operator == ">=" {
+		if requestedTemperature > minTemperature {
+			minTemperature = requestedTemperature
+		}
+	}
+
+	if operator == "<=" {
+		if requestedTemperature < maxTemperature {
+			maxTemperature = requestedTemperature
+		}
+	}
+
+	return minTemperature, maxTemperature
+}
 
 func main() {
-	var departmentsCount, employeesPerDepartment int
+	var departments int
 
-	if _, err := fmt.Scan(&departmentsCount); err != nil {
+	if _, err := fmt.Scan(&departments); err != nil {
 		return
 	}
 
-	if _, err := fmt.Scan(&employeesPerDepartment); err != nil {
+	if departments < MinValidNumber || departments > MaxValidNumber {
 		return
 	}
 
-	for range departmentsCount {
-		minTemperature := 15
-		maxTemperature := 30
+	for range departments {
+		minTemperature := MinTemp
+		maxTemperature := MaxTemp
 
-		for range employeesPerDepartment {
-			var condition string
+		var employees int
+
+		if _, err := fmt.Scan(&employees); err != nil {
+			return
+		}
+
+		if employees < MinValidNumber || employees > MaxValidNumber {
+			return
+		}
+
+		for range employees {
+			var operator string
 			var requestedTemperature int
 
-			if _, err := fmt.Scan(&condition, &requestedTemperature); err != nil {
+			if _, err := fmt.Scan(&operator, &requestedTemperature); err != nil {
 				return
 			}
 
-			if condition == ">=" && requestedTemperature > minTemperature {
-				minTemperature = requestedTemperature
-			}
+			minTemperature, maxTemperature = findOptimal(minTemperature, maxTemperature, operator, requestedTemperature)
 
-			if condition == "<=" && requestedTemperature < maxTemperature {
-				maxTemperature = requestedTemperature
-			}
-
-			if minTemperature <= maxTemperature && minTemperature >= 15 && maxTemperature <= 30 {
+			if minTemperature <= maxTemperature {
 				fmt.Println(minTemperature)
 			} else {
 				fmt.Println(-1)
