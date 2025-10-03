600da4b545660f69c3fdaa2a6e94c986b23afc52
Expeline <kalashyan.artur@list.ru>
2025-10-03 16:48:35 +0300

    [TASK-2-1] Update code format for lint and fix cycle

--- arthur.kalashyan/task-2-1/cmd/service/main.go
+++ arthur.kalashyan/task-2-1/cmd/service/main.go
@@ -4,36 +4,40 @@ import "fmt"
 
 func main() {
 	var departmentsCount, employeesPerDepartment int
+
 	if _, err := fmt.Scan(&departmentsCount); err != nil {
 		return
 	}
+
 	if _, err := fmt.Scan(&employeesPerDepartment); err != nil {
 		return
 	}
 
-	totalEmployees := departmentsCount * employeesPerDepartment
-	minTemperature, maxTemperature := 15, 30
+	for departament := 0; departament < departmentsCount; departament++ {
+		minTemperature := 15
+		maxTemperature := 30
 
-	for i := 0; i < totalEmployees; i++ {
-		var condition string
-		var requestedTemperature int
+		for employee := 0; employee < employeesPerDepartment; employee++ {
+			var condition string
+			var requestedTemperature int
 
-		if _, err := fmt.Scan(&condition, &requestedTemperature); err != nil {
-			return
-		}
+			if _, err := fmt.Scan(&condition, &requestedTemperature); err != nil {
+				return
+			}
 
-		if condition == ">=" && requestedTemperature > minTemperature {
-			minTemperature = requestedTemperature
-		}
+			if condition == ">=" && requestedTemperature > minTemperature {
+				minTemperature = requestedTemperature
+			}
 
-		if condition == "<=" && requestedTemperature < maxTemperature {
-			maxTemperature = requestedTemperature
-		}
+			if condition == "<=" && requestedTemperature < maxTemperature {
+				maxTemperature = requestedTemperature
+			}
 
-		if minTemperature <= maxTemperature {
-			fmt.Println(minTemperature)
-		} else {
-			fmt.Println(-1)
+			if minTemperature <= maxTemperature {
+				fmt.Println(minTemperature)
+			} else {
+				fmt.Println(-1)
+			}
 		}
 	}
 }
