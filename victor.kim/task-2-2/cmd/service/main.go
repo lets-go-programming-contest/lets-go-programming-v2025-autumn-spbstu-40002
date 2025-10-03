b659f84c99dcef632cc268e01bce3e4f0b178351
л1ьмщ <kimvo1511@gmail.com>
2025-10-03 23:57:07 +0300

    [TASK-2-2] Initial commit for task2-2 attempt 8

--- victor.kim/task-2-2/cmd/service/main.go
+++ victor.kim/task-2-2/cmd/service/main.go
@@ -8,15 +8,39 @@ import (
 
 type IntHeap []int
 
-func (h IntHeap) Len() int           { return len(h) }
-func (h IntHeap) Less(i, j int) bool { return h[i] > h[j] } // Максимальная куча
-func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-func (h *IntHeap) Push(x any)        { *h = append(*h, x.(int)) }
+func (h *IntHeap) Len() int {
+	return len(*h)
+}
+
+func (h *IntHeap) Less(i, j int) bool {
+	return (*h)[i] > (*h)[j] // максимальная куча
+}
+
+func (h *IntHeap) Swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
+}
+
+func (h *IntHeap) Push(x any) {
+	value, ok := x.(int)
+	if !ok {
+		fmt.Fprintln(os.Stderr, "Push: type assertion failed")
+
+		return
+	}
+
+	*h = append(*h, value)
+}
+
 func (h *IntHeap) Pop() any {
 	old := *h
 	n := len(old)
+	if n == 0 {
+		return nil
+	}
 	x := old[n-1]
-	*h = old[0 : n-1]
+
+	*h = old[:n-1]
+
 	return x
 }
 
@@ -24,33 +48,40 @@ func main() {
 	var dishCount int
 	if _, err := fmt.Scan(&dishCount); err != nil {
 		fmt.Fprintln(os.Stderr, "couldn't read the number of dishes:", err)
+
 		return
 	}
 
 	dishes := &IntHeap{}
-	for i := 0; i < dishCount; i++ {
+	heap.Init(dishes)
+
+	for i := range make([]struct{}, dishCount) {
 		var dish int
 		if _, err := fmt.Scan(&dish); err != nil {
 			fmt.Fprintln(os.Stderr, "couldn't read dish preference:", err)
+
 			return
 		}
+
 		heap.Push(dishes, dish)
 	}
 
-	var k int
-	if _, err := fmt.Scan(&k); err != nil {
-		fmt.Fprintln(os.Stderr, "couldn't read k:", err)
-		return
-	}
+	var preferredDishIndex int
+	if _, err := fmt.Scan(&preferredDishIndex); err != nil || dishes.Len() < preferredDishIndex {
+		fmt.Fprintln(os.Stderr, "invalid preferred dish index")
 
-	if dishes.Len() < k {
-		fmt.Fprintln(os.Stderr, "k is larger than the number of dishes")
 		return
 	}
 
 	var selected int
-	for i := 0; i < k; i++ {
-		selected = heap.Pop(dishes).(int)
+	for i := range make([]struct{}, preferredDishIndex) {
+		value, ok := heap.Pop(dishes).(int)
+		if !ok {
+			fmt.Fprintln(os.Stderr, "Pop: type assertion failed")
+
+			return
+		}
+		selected = value
 	}
 
 	fmt.Println(selected)
