68cc5ab010ae8a3b256665da4b0b34f303c2837c
л1ьмщ <kimvo1511@gmail.com>
2025-10-04 00:18:37 +0300

    [TASK-2-2] Initial commit for task2-2 attempt 12

--- victor.kim/task-2-2/cmd/service/main.go
+++ victor.kim/task-2-2/cmd/service/main.go
@@ -3,92 +3,67 @@ package main
 import (
 	"container/heap"
 	"fmt"
-	"os"
 )
 
-type DishHeap []int
+type IntHeap []int
 
-func (h *DishHeap) Len() int {
-	return len(*h)
-}
+func (h *IntHeap) Len() int           { return len(*h) }
+func (h *IntHeap) Less(i, j int) bool { return (*h)[i] < (*h)[j] }
+func (h *IntHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
 
-func (h *DishHeap) Less(i, j int) bool {
-	return (*h)[i] > (*h)[j] // максимальная куча
-}
-
-func (h *DishHeap) Swap(i, j int) {
-	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
-}
-
-func (h *DishHeap) Push(x any) {
-	var dish int
-	var ok bool
-
-	if dish, ok = x.(int); !ok {
-		fmt.Fprintln(os.Stderr, "Push: type assertion failed")
-
-		return
+func (h *IntHeap) Push(x interface{}) {
+	value, ok := x.(int)
+	if !ok {
+		panic("IntHeap: Push expects int")
 	}
 
-	*h = append(*h, dish)
+	*h = append(*h, value)
 }
 
-func (h *DishHeap) Pop() any {
-	old := *h
-	n := len(old)
-	if n == 0 {
+func (h *IntHeap) Pop() interface{} {
+	oldHeap := *h
+	heapSize := len(oldHeap)
+	if heapSize == 0 {
 		return nil
 	}
-	dish := old[n-1]
 
-	*h = old[:n-1]
+	lastElement := oldHeap[heapSize-1]
+	*h = oldHeap[:heapSize-1]
 
-	return dish
+	return lastElement
 }
 
 func main() {
-	var totalDishes uint16
-
-	if _, err := fmt.Scanln(&totalDishes); err != nil || totalDishes == 0 || totalDishes > 10000 {
-		fmt.Println("invalid number of dishes")
-
+	var total int
+	if _, err := fmt.Scan(&total); err != nil || total <= 0 {
 		return
 	}
 
-	dishesHeap := &DishHeap{}
-	heap.Init(dishesHeap)
-
-	for range make([]struct{}, totalDishes) {
-		var preference int
-
-		if _, err := fmt.Scan(&preference); err != nil {
-			fmt.Fprintln(os.Stderr, "couldn't read dish preference:", err)
-
+	array := make([]int, total)
+	for index := range array {
+		if _, err := fmt.Scan(&array[index]); err != nil {
 			return
 		}
-
-		heap.Push(dishesHeap, preference)
 	}
 
-	var preferredRank uint16
-
-	if _, err := fmt.Scan(&preferredRank); err != nil || preferredRank == 0 || dishesHeap.Len() < int(preferredRank) {
-		fmt.Println("invalid preferred dish rank")
-
+	var kth int
+	if _, err := fmt.Scan(&kth); err != nil || kth <= 0 || kth > total {
 		return
 	}
 
-	var selectedDish int
+	minHeap := &IntHeap{}
+	heap.Init(minHeap)
 
-	for range make([]struct{}, preferredRank) {
-		value, ok := heap.Pop(dishesHeap).(int)
-		if !ok {
-			fmt.Fprintln(os.Stderr, "Pop: type assertion failed")
-
-			return
-		}
-		selectedDish = value
+	for index := range array[:kth] {
+		heap.Push(minHeap, array[index])
 	}
 
-	fmt.Println(selectedDish)
+	for _, value := range array[kth:] {
+		if value > (*minHeap)[0] {
+			heap.Pop(minHeap)
+			heap.Push(minHeap, value)
+		}
+	}
+
+	fmt.Println((*minHeap)[0])
 }
