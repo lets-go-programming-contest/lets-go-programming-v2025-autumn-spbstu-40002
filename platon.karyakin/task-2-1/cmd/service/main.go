307e762b768e3e4f48a82c46d705baff80bb54c8
Карякин Платон <viceone.von@gmail.com>
2025-10-04 11:01:40 +0300

    [TASK-2-1] fixed linter warnings 2

--- platon.karyakin/task-2-1/cmd/service/main.go
+++ platon.karyakin/task-2-1/cmd/service/main.go
@@ -14,36 +14,38 @@ const (
 
 var errFormat = errors.New("invalid input format or value")
 
-
 func readOperationTemp() (string, int, error) {
 	var (
 		operation string
 		temp      int
 	)
+
 	_, err := fmt.Scanln(&operation, &temp)
 	if err != nil {
 		return "", 0, errFormat
 	}
+
 	if operation != ">=" && operation != "<=" {
 		return "", 0, errFormat
 	}
+
 	if temp < initialMinTemp || temp > initialMaxTemp {
 		return "", 0, errFormat
 	}
+
 	return operation, temp, nil
 }
 
 func main() {
 	var departmentsCount int
+
 	if _, err := fmt.Fscan(os.Stdin, &departmentsCount); err != nil {
 		return
 	}
 
-
-	for departmentIndex := range departmentsCount {
-		_ = departmentIndex 
-
+	for range departmentsCount {
 		var employeesCount int
+
 		if _, err := fmt.Fscan(os.Stdin, &employeesCount); err != nil {
 			return
 		}
@@ -51,13 +53,12 @@ func main() {
 		currentMinTemp := initialMinTemp
 		currentMaxTemp := initialMaxTemp
 
-		for employeeIndex := range employeesCount {
-			_ = employeeIndex
-
+		for range employeesCount {
 			operation, temperature, err := readOperationTemp()
 			if err != nil {
 				log.Fatalf("error: %v", err)
 			}
+
 			if operation == ">=" {
 				if temperature > currentMinTemp {
 					currentMinTemp = temperature
@@ -67,6 +68,7 @@ func main() {
 					currentMaxTemp = temperature
 				}
 			}
+
 			if currentMinTemp <= currentMaxTemp {
 				fmt.Println(currentMinTemp)
 			} else {
