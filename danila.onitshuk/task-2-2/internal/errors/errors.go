2f7395e9ad09df82f45085412cdb3c7a6575adbb
danila-clown <daphetad@gmail.com>
2025-10-04 03:01:49 +0000

    [TASK-2-2] fix package inputerror

--- danila.onitshuk/task-2-2/internal/errors/errors.go
+++ danila.onitshuk/task-2-2/internal/errors/errors.go
@@ -1,10 +1,10 @@
-package inputError
+package inputerror
 
 import "errors"
 
 var (
-	ErrInvalidNumberDishes    = errors.New("The number of dishes on the table should be set from 1 to 10,000")
-	ErrInvalidPriorityDish    = errors.New("The priority of the dish should be set from -10000 to 10000")
-	ErrInvalidPreferredDishID = errors.New("The ID of the preferred dish should be no more than the number of dishes themselves.")
-	ErrInvalidTypeData        = errors.New("Invalid data type")
+	ErrInvalidNumberDishes    = errors.New("the number of dishes on the table should be set from 1 to 10,000")
+	ErrInvalidPriorityDish    = errors.New("the priority of the dish should be set from -10000 to 10000")
+	ErrInvalidPreferredDishID = errors.New("the ID of the preferred dish should be no more than the number of dishes themselves.")
+	ErrInvalidTypeData        = errors.New("invalid data type")
 )
--- danila.onitshuk/task-2-2/internal/service/agony_choice.go
+++ danila.onitshuk/task-2-2/internal/service/agony_choice.go
@@ -4,7 +4,7 @@ import (
 	"container/heap"
 	"fmt"
 
-	inputError "danila.onitshuk/task-2-2/internal/errors"
+	inputerror "danila.onitshuk/task-2-2/internal/errors"
 	interfaces "danila.onitshuk/task-2-2/internal/heap"
 )
 
@@ -19,11 +19,11 @@ func AgonyChoice() {
 
 	_, err := fmt.Scan(&dishCount)
 	if err != nil {
-		fmt.Println(inputError.ErrInvalidTypeData)
+		fmt.Println(inputerror.ErrInvalidTypeData)
 
 		return
 	} else if dishCount < 1 || dishCount > 10_000 {
-		fmt.Println(inputError.ErrInvalidNumberDishes)
+		fmt.Println(inputerror.ErrInvalidNumberDishes)
 
 		return
 	}
@@ -33,11 +33,11 @@ func AgonyChoice() {
 
 		_, err := fmt.Scan(&dish)
 		if err != nil {
-			fmt.Println(inputError.ErrInvalidTypeData)
+			fmt.Println(inputerror.ErrInvalidTypeData)
 
 			return
 		} else if dish < -10_000 || dish > 10_000 {
-			fmt.Println(inputError.ErrInvalidPriorityDish)
+			fmt.Println(inputerror.ErrInvalidPriorityDish)
 
 			return
 		}
@@ -47,11 +47,11 @@ func AgonyChoice() {
 
 	_, err = fmt.Scan(&dishPreferenceID)
 	if err != nil {
-		fmt.Println(inputError.ErrInvalidTypeData)
+		fmt.Println(inputerror.ErrInvalidTypeData)
 
 		return
 	} else if dishPreferenceID < 1 || dishPreferenceID > dishCount {
-		fmt.Println(inputError.ErrInvalidNumberDishes)
+		fmt.Println(inputerror.ErrInvalidNumberDishes)
 
 		return
 	}
