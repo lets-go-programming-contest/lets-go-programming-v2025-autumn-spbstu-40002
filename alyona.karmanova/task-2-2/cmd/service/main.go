f18e337939f03b0eed3231e012e5cbfa375188e5
Karmanova Alyona <karaliona2006@gmail.com>
2025-10-09 20:28:45 +0300

    [TASK-2-2] fix mistake

--- alyona.karmanova/task-2-2/cmd/service/main.go
+++ alyona.karmanova/task-2-2/cmd/service/main.go
@@ -3,59 +3,34 @@ package main
 import (
 	"container/heap"
 	"fmt"
+
+	myheap "github.com/HuaChenju/task-2-2/iternal/heap"
 )
 
-type MaxHeap []int
-
-func (h *MaxHeap) Len() int {
-	return len(*h)
-}
-
-func (h *MaxHeap) Less(i, j int) bool {
-	return (*h)[i] > (*h)[j]
-}
-
-func (h *MaxHeap) Swap(i, j int) {
-	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
-}
-
-func (h *MaxHeap) Push(elem interface{}) {
-	val, err := elem.(int)
-
-	if !err {
-		return
-	}
-
-	*h = append(*h, val)
-}
-
-func (h *MaxHeap) Pop() interface{} {
-	oldHeap := *h
-
-	n := len(oldHeap)
-
-	maxElem := oldHeap[n-1]
-
-	*h = oldHeap[0 : n-1]
-
-	return maxElem
-}
+const (
+	MinCount      = 1
+	MaxCount      = 10000
+	MinRating     = -10000
+	MaxRating     = 10000
+	MinPreference = 1
+)
 
 func main() {
 	var count, preference, rating int
 
-	heapMax := &MaxHeap{}
+	heapMax := &myheap.MaxHeap{}
 
 	_, err := fmt.Scan(&count)
 
-	if err != nil || count < 1 || count > 10000 {
+	if err != nil || count < MinCount || count > MinCount {
+		fmt.Println("incorrect amount of dishes")
 		return
 	}
 
 	for range count {
 		_, err = fmt.Scan(&rating)
 
-		if err != nil || rating < -10000 || rating > 10000 {
+		if err != nil || rating < MinRating || rating > MaxRating {
 			return
 		}
 
@@ -64,13 +39,22 @@ func main() {
 
 	_, err = fmt.Scan(&preference)
 
-	if err != nil || preference > count {
+	if err != nil || preference > count || preference < MinPreference {
 		return
 	}
 
 	for range preference - 1 {
-		heap.Pop(heapMax)
+		val := heap.Pop(heapMax)
+		if val == nil {
+			fmt.Println("Heap is empty unexpectedly")
+			return
+		}
 	}
 
-	fmt.Println(heap.Pop(heapMax))
+	val := heap.Pop(heapMax)
+	if val == nil {
+		fmt.Println("Heap is empty unexpectedly")
+		return
+	}
+	fmt.Println(val)
 }
