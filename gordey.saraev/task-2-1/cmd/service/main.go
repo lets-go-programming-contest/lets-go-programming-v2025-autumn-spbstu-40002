218d6148a92a0734d0036fe655d50a2d76665490
Сараев Гордей <isisu7337@gmail.com>
2025-10-09 19:28:59 +0300

    [Task-2-1] Another attempt to fix lint x2

--- gordey.saraev/task-2-1/cmd/service/main.go
+++ gordey.saraev/task-2-1/cmd/service/main.go
@@ -14,16 +14,19 @@ func processDepartment(employees int) {
 		_, err := fmt.Scanln(&operator, &temperature)
 		if err != nil {
 			fmt.Println("Error reading operator and temperature")
+
 			return
 		}
 
 		if operator != ">=" && operator != "<=" {
 			fmt.Println("Invalid operator. Must be '>=' or '<='")
+
 			return
 		}
 
 		if temperature < 15 || temperature > 30 {
 			fmt.Println("Temperature is out of range [15, 30]")
+
 			return
 		}
 
@@ -51,11 +54,13 @@ func main() {
 	_, err := fmt.Scanln(&departments)
 	if err != nil {
 		fmt.Println("Error reading departments count")
+
 		return
 	}
 
 	if departments < 1 || departments > 1000 {
 		fmt.Println("Departments is out of range [1, 1000]")
+
 		return
 	}
 
@@ -64,11 +69,13 @@ func main() {
 		_, err = fmt.Scanln(&employees)
 		if err != nil {
 			fmt.Println("Error reading employees count")
+
 			return
 		}
 
 		if employees < 1 || employees > 1000 {
 			fmt.Println("Employees is out of range [1, 1000]")
+
 			return
 		}
 
