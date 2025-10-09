03c0378bcefd91d44320e12766d4c5452d09385c
Ivashin <danya001406@gmail.com>
2025-10-09 19:19:07 +0300

    [TASK-2-1] Add solution

---
+++ danya.ivashin/task-2-1/cmd/service/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+const (
+	minTemperature = 15
+	maxTemperature = 30
+)
+
+var errFormat = errors.New("invalid temperature format")
+
+func adjustTemperature(low int, high int) (int, int, error) {
+	var op string
+	var t int
+
+	_, err := fmt.Scanln(&op, &t)
+	if err != nil || t < minTemperature || t > maxTemperature {
+		return 0, 0, errFormat
+	}
+
+	switch op {
+	case ">=":
+		if high < t {
+			return -1, -1, nil
+		}
+		if low < t {
+			low = t
+		}
+	case "<=":
+		if low > t {
+			return -1, -1, nil
+		}
+		if high > t {
+			high = t
+		}
+	default:
+		return 0, 0, errFormat
+	}
+
+	return low, high, nil
+}
+
+func main() {
+	var departments, employees int
+
+	_, err := fmt.Scanln(&departments)
+	if err != nil || departments < 1 || departments > 1000 {
+		fmt.Println("invalid number of departments")
+		return
+	}
+
+	for d := 0; d < departments; d++ {
+		_, err = fmt.Scanln(&employees)
+		if err != nil || employees < 1 || employees > 1000 {
+			fmt.Println("invalid number of employees")
+			return
+		}
+
+		low := minTemperature
+		high := maxTemperature
+
+		for e := 0; e < employees; e++ {
+			low, high, err = adjustTemperature(low, high)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			if low == -1 || high == -1 || low > high {
+				fmt.Println(-1)
+			} else {
+				fmt.Println(low)
+			}
+		}
+	}
+}
