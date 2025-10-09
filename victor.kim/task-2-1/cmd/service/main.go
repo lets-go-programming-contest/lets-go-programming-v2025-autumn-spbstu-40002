9fefa9488acefe87c483dc46de836731aae13fbe
л1ьмщ <kimvo1511@gmail.com>
2025-10-09 15:56:56 +0300

    [TASK-2-1] Initial commit for task2-1 attempt 13

--- victor.kim/task-2-1/cmd/service/main.go
+++ victor.kim/task-2-1/cmd/service/main.go
@@ -10,77 +10,154 @@ const (
 	maxTemperature = 30
 )
 
-var errFormat = errors.New("invalid temperature format")
+var (
+	errFormat     = errors.New("invalid temperature format")
+	errScanFailed = errors.New("scan failed")
+)
 
-func adjustTemperature(low int, high int) (int, int, error) {
-	var (
-		operation string
-		newTemp   int
-	)
+type Thermostat struct {
+	low      int
+	high     int
+	conflict bool
+}
 
-	_, err := fmt.Scanln(&operation, &newTemp)
-	if err != nil || newTemp < minTemperature || newTemp > maxTemperature {
-		return 0, 0, errFormat
+func NewThermostat() *Thermostat {
+	return &Thermostat{
+		low:  minTemperature,
+		high: maxTemperature,
+	}
+}
+
+func (t *Thermostat) Update(operation string, value int) (bool, error) {
+	if value < minTemperature || value > maxTemperature {
+		return false, errFormat
+	}
+
+	if t.conflict {
+		return true, nil
 	}
 
 	switch operation {
 	case ">=":
-		if high < newTemp {
-			return -1, -1, nil
+		if value > t.high {
+			t.conflict = true
+			return true, nil
 		}
 
-		if low < newTemp {
-			low = newTemp
+		if value > t.low {
+			t.low = value
 		}
 	case "<=":
-		if low > newTemp {
-			return -1, -1, nil
+		if value < t.low {
+			t.conflict = true
+			return true, nil
 		}
 
-		if high > newTemp {
-			high = newTemp
+		if value < t.high {
+			t.high = value
 		}
 	default:
-		return 0, 0, errFormat
+		return false, errFormat
 	}
 
-	return low, high, nil
+	return t.conflict, nil
+}
+
+func (t *Thermostat) Current() (int, error) {
+	if t.conflict {
+		return -1, nil
+	}
+
+	return t.low, nil
+}
+
+func readInt() (int, error) {
+	var value int
+	scanCount, scanErr := fmt.Scan(&value)
+	if scanErr != nil {
+		return 0, errScanFailed
+	}
+
+	if scanCount != 1 {
+		return 0, errScanFailed
+	}
+
+	return value, nil
+}
+
+func readOperationAndValue() (string, int, error) {
+	var operation string
+	var value int
+
+	scanCount, scanErr := fmt.Scan(&operation, &value)
+	if scanErr != nil {
+		return "", 0, errScanFailed
+	}
+
+	if scanCount != 2 {
+		return "", 0, errScanFailed
+	}
+
+	return operation, value, nil
+}
+
+func processDepartment(employees int) error {
+	therm := NewThermostat()
+
+	for range make([]struct{}, employees) {
+		operation, value, readErr := readOperationAndValue()
+		if readErr != nil {
+			return readErr
+		}
+
+		conflict, updateErr := therm.Update(operation, value)
+		if updateErr != nil {
+			return updateErr
+		}
+
+		if conflict {
+			fmt.Println(-1)
+			continue
+		}
+
+		temp, curErr := therm.Current()
+		if curErr != nil {
+			return curErr
+		}
+
+		fmt.Println(temp)
+	}
+
+	return nil
 }
 
 func main() {
-	var dep, emp uint16
-
-	_, err := fmt.Scanln(&dep)
-	if err != nil || dep == 0 || dep > 1000 {
-		fmt.Println("invalid number of departments")
-
+	departments, err := readInt()
+	if err != nil {
+		fmt.Println(err)
 		return
 	}
 
-	for range dep {
-		_, err = fmt.Scanln(&emp)
-		if err != nil || emp == 0 || emp > 1000 {
-			fmt.Println("invalid number of employees")
+	if departments <= 0 || departments > 1000 {
+		fmt.Println("invalid number of departments")
+		return
+	}
 
+	for range make([]struct{}, departments) {
+		employees, err := readInt()
+		if err != nil {
+			fmt.Println(err)
 			return
 		}
 
-		lowTemp := minTemperature
-		highTemp := maxTemperature
+		if employees <= 0 || employees > 1000 {
+			fmt.Println("invalid number of employees")
+			return
+		}
 
-		for range emp {
-			lowTemp, highTemp, err = adjustTemperature(lowTemp, highTemp)
-			if err != nil {
-				fmt.Println(err)
-
-				return
-			}
-
-			if lowTemp == -1 && highTemp == -1 {
-				fmt.Println(-1)
-			} else {
-				fmt.Println(lowTemp)
-			}
+		if procErr := processDepartment(employees); procErr != nil {
+			fmt.Println(procErr)
+			return
 		}
 	}
 }
