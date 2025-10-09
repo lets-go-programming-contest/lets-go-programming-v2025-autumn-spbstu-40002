f9d49b03a189e5dcb77ee06a65d64d66f5d7d22f
sonychello <sofapozneeva@gmail.com>
2025-10-10 00:34:10 +0300

    [TASK-2-1] Update main.go

--- sofya.pozneeva/task-2-1/cmd/service/main.go
+++ sofya.pozneeva/task-2-1/cmd/service/main.go
@@ -4,17 +4,11 @@ import "fmt"
 
 func main() {
 	var minTemp uint8
-	
 	var maxTemp uint8
-	
 	var nSection uint8
-	
 	var nPeople uint8
-	
 	var sign string
-	
 	var temp uint8
-	
 	var err error
 	
 	_, err = fmt.Scanln(&nSection)
