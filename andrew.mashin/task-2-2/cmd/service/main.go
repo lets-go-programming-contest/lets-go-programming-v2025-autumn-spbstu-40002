0a0af79b7669d8822f11e3af56663a22062dfe77
Andrew <97516045+Exam-Play@users.noreply.github.com>
2025-10-09 23:09:14 +0300

    [TASK-2-2] Fix build

--- andrew.mashin/task-2-2/cmd/service/main.go
+++ andrew.mashin/task-2-2/cmd/service/main.go
@@ -15,46 +15,11 @@ const (
 	ratingMax         = 10000
 )
 
-<<<<<<< HEAD
 var (
 	errorIncorrectNumberOfDishes = errors.New("incorrect number of dishes")
 	errorIncorrectRating         = errors.New("incorrect rating")
 	errorIncorrectK              = errors.New("incorrect preferred dish")
 )
-=======
-type MyHeap []int
-
-func (h *MyHeap) Len() int {
-	return len(*h)
-}
-
-func (h *MyHeap) Less(i, j int) bool {
-	return (*h)[i] < (*h)[j]
-}
-
-func (h *MyHeap) Swap(i, j int) {
-	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
-}
-
-func (h *MyHeap) Push(x interface{}) {
-	if v, ok := x.(int); ok {
-		*h = append(*h, v)
-	}
-}
-
-func (h *MyHeap) Pop() interface{} {
-	if len(*h) == 0 {
-		return nil
-	}
-
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
-
-	return x
-}
->>>>>>> 6adc7d426de11c732732c49f0c72684de4a324df
 
 func main() {
 	var numberOfDishes int
