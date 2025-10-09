e892412455f9fd102fa58e99dd8b32363c617375
t1wt07 <t1wt.work@gmail.com>
2025-10-09 18:45:23 +0300

    [TASK-2-2] Initial commit

---
+++ timur.usachev/task-2-2/cmd/service/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"container/heap"
+	"fmt"
+	"os"
+)
+
+type IntHeap []int
+
+func (h IntHeap) Len() int {
+	return len(h)
+}
+
+func (h IntHeap) Less(i, j int) bool {
+	return h[i] < h[j]
+}
+
+func (h IntHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[:n-1]
+	return x
+}
+
+func main() {
+	var count int
+	if _, err := fmt.Fscan(os.Stdin, &count); err != nil || count <= 0 {
+		return
+	}
+
+	values := make([]int, count)
+	index := 0
+	for range make([]struct{}, count) {
+		if _, err := fmt.Fscan(os.Stdin, &values[index]); err != nil {
+			return
+		}
+		index++
+	}
+
+	var kth int
+	if _, err := fmt.Fscan(os.Stdin, &kth); err != nil || kth <= 0 || kth > count {
+		return
+	}
+
+	minHeap := &IntHeap{}
+	heap.Init(minHeap)
+
+	index = 0
+	for range make([]struct{}, kth) {
+		heap.Push(minHeap, values[index])
+		index++
+	}
+
+	for range make([]struct{}, count-kth) {
+		val := values[index]
+		if val > (*minHeap)[0] {
+			heap.Pop(minHeap)
+			heap.Push(minHeap, val)
+		}
+		index++
+	}
+
+	top := (*minHeap)[0]
+	fmt.Println(top)
+}
