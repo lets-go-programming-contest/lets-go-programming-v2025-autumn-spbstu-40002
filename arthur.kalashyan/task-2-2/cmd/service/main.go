d4410e8da83a800dc54cc62956d53b2568992780
Expeline <kalashyan.artur@list.ru>
2025-10-10 15:30:49 +0300

    [TASK-2-2] Create structure heap

--- arthur.kalashyan/task-2-2/cmd/service/main.go
+++ arthur.kalashyan/task-2-2/cmd/service/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"container/heap"
 	"fmt"
+
+	"github.com/Expeline/task-2-2/internal/heap"
 )
 
 const (
@@ -11,58 +12,6 @@ const (
 	MinN     = 1
 	MaxN     = 10000
 )
-
-type MinHeap []int
-
-func (h *MinHeap) Len() int {
-	return len(*h)
-}
-
-func (h *MinHeap) Less(i, j int) bool {
-	return (*h)[i] < (*h)[j]
-}
-
-func (h *MinHeap) Swap(i, j int) {
-	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
-}
-
-func (h *MinHeap) Push(x any) {
-	value, ok := x.(int)
-
-	if !ok {
-		return
-	}
-
-	*h = append(*h, value)
-}
-
-func (h *MinHeap) Pop() any {
-	old := *h
-
-	n := len(old)
-
-	x := old[n-1]
-
-	*h = old[:n-1]
-
-	return x
-}
-
-func findKthLargest(dishes []int, kth int) int {
-	minHeap := &MinHeap{}
-
-	heap.Init(minHeap)
-
-	for _, dish := range dishes {
-		heap.Push(minHeap, dish)
-
-		if minHeap.Len() > kth {
-			heap.Pop(minHeap)
-		}
-	}
-
-	return (*minHeap)[0]
-}
 
 func main() {
 	var dishesCount int
@@ -101,7 +50,7 @@ func main() {
 		return
 	}
 
-	result := findKthLargest(dishes, kthDish)
+	result := heap.FindKthLargest(dishes, kthDish)
 
 	fmt.Println(result)
 }
---
+++ arthur.kalashyan/task-2-2/internal/heap/heap.go
@@ -0,0 +1,55 @@
+package heap
+
+import "container/heap"
+
+type MinHeap []int
+
+func (h MinHeap) Len() int {
+	return len(h)
+}
+
+func (h MinHeap) Less(i, j int) bool {
+	return h[i] < h[j]
+}
+func (h MinHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *MinHeap) Push(x any) {
+	value, ok := x.(int)
+
+	if !ok {
+		panic("heap.Push: value is not int")
+	}
+
+	*h = append(*h, value)
+}
+
+func (h *MinHeap) Pop() any {
+	old := *h
+	n := len(old)
+
+	if n == 0 {
+		panic("heap.Pop: empty heap")
+	}
+
+	x := old[n-1]
+	*h = old[:n-1]
+	return x
+}
+
+func FindKthLargest(dishes []int, kth int) int {
+	minHeap := &MinHeap{}
+
+	heap.Init(minHeap)
+
+	for _, dish := range dishes {
+		heap.Push(minHeap, dish)
+
+		if minHeap.Len() > kth {
+			heap.Pop(minHeap)
+		}
+	}
+
+	return (*minHeap)[0]
+}
