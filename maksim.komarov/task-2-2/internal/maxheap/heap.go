ee75471fcfe5247c041337fb1110d2264ccee55c
MeguruMacabre <megurumacabre@gmail.com>
2025-10-10 16:18:03 +0300

    [TASK-2-2] K-th Preferred Dish

---
+++ maksim.komarov/task-2-2/internal/maxheap/heap.go
@@ -0,0 +1,18 @@
+package maxheap
+
+type MaxHeap []int
+
+func (h MaxHeap) Len() int           { return len(h) }
+func (h MaxHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h MaxHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *MaxHeap) Push(x any) {
+	*h = append(*h, x.(int))
+}
+
+func (h *MaxHeap) Pop() any {
+	old := *h
+	v := old[len(old)-1]
+	*h = old[:len(old)-1]
+	return v
+}
