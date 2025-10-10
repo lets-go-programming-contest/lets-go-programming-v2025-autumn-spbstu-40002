6aaad66cfaa68c5eb8a4184b947decb65201fc43
danila-clown <daphetad@gmail.com>
2025-10-10 00:28:41 +0000

    [TASK-2-1] correction of comments

--- danila.onitshuk/task-2-1/cmd/service/main.go
+++ danila.onitshuk/task-2-1/cmd/service/main.go
@@ -1,57 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-func department() {
-	minTemperature := 15
-	maxTemperature := 30
-
-	var employeesCount int
-
-	_, err := fmt.Scan(&employeesCount)
-	if err != nil {
-		return
-	}
-
-	for range employeesCount {
-		var (
-			sign        string
-			temperature int
-		)
-
-		_, err := fmt.Scan(&sign, &temperature)
-		if err != nil {
-			return
-		}
-
-		switch sign {
-		case ">=":
-			if temperature > minTemperature {
-				minTemperature = temperature
-			}
-		case "<=":
-			if temperature < maxTemperature {
-				maxTemperature = temperature
-			}
-		}
-
-		if minTemperature <= maxTemperature && minTemperature >= 15 && maxTemperature <= 30 {
-			fmt.Println(minTemperature)
-		} else {
-			fmt.Println(-1)
-		}
-	}
-}
+	department "danila.onitshuk/task-2-1/internal/department"
+)
 
 func main() {
 	var departmentsCount int
 
 	_, err := fmt.Scan(&departmentsCount)
 	if err != nil {
+		fmt.Println(department.ErrInvalidTypeData)
+
+		return
+	} else if departmentsCount < department.MinimumCountDepartments || departmentsCount > department.MaximumCountDepartments {
+		fmt.Println(department.ErrCountDeparments)
+
 		return
 	}
 
 	for range departmentsCount {
-		department()
+		var employeesCount int
+
+		_, err := fmt.Scan(&employeesCount)
+		if err != nil {
+			fmt.Println(department.ErrInvalidTypeData)
+
+			return
+		} else if employeesCount < department.MinimumCountEmployees || employeesCount > department.MaximumCountEmployees {
+			fmt.Println(department.ErrcountEmployees)
+
+			return
+		}
+
+		currentDepartment := department.NewDepartment()
+
+		for range employeesCount {
+			var (
+				sign        string
+				temperature int
+			)
+
+			_, err := fmt.Scan(&sign, &temperature)
+			if err != nil {
+				fmt.Println(department.ErrInvalidTypeData)
+
+				return
+			}
+
+			err = currentDepartment.ReduceRange(sign, temperature)
+			if err != nil {
+				fmt.Println(err)
+
+				return
+			}
+		}
 	}
 }
