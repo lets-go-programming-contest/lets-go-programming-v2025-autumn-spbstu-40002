9ff6ffa0f1bb0a8b2a9d43fea2b61d9637a52e47
MeguruMacabre <megurumacabre@gmail.com>
2025-10-10 16:23:48 +0300

    [TASK-2-2] K-th Preferred Dish FIX LINT

--- maksim.komarov/task-2-2/internal/maxheap/heap.go
+++ maksim.komarov/task-2-2/internal/maxheap/heap.go
@@ -2,17 +2,27 @@ package maxheap
 
 type MaxHeap []int
 
-func (h MaxHeap) Len() int           { return len(h) }
-func (h MaxHeap) Less(i, j int) bool { return h[i] > h[j] }
-func (h MaxHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h *MaxHeap) Len() int           { return len(*h) }
+func (h *MaxHeap) Less(i, j int) bool { return (*h)[i] > (*h)[j] }
+func (h *MaxHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
 
 func (h *MaxHeap) Push(x any) {
-	*h = append(*h, x.(int))
+	v, ok := x.(int)
+	if !ok {
+		return
+	}
+
+	*h = append(*h, v)
 }
 
 func (h *MaxHeap) Pop() any {
 	old := *h
+	if len(old) == 0 {
+		return 0
+	}
+
 	v := old[len(old)-1]
 	*h = old[:len(old)-1]
+
 	return v
 }
