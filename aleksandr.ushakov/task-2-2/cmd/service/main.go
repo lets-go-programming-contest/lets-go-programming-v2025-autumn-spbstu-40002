b7b30b6447e578991bfe38d374e3e46835f9694d
“Ушаков <aleksandrush06@gmail.com>
2025-10-10 13:21:41 +0300

    [TASK-2-2] Added new package

--- aleksandr.ushakov/task-2-2/cmd/service/main.go
+++ aleksandr.ushakov/task-2-2/cmd/service/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"container/heap"
 	"fmt"
+
+	"github.com/rachguta/task-2-2/myHeap"
 )
 
 const (
@@ -11,31 +13,6 @@ const (
 	minMark        = -10000
 	maxMark        = 10000
 )
-
-// Heap realization.
-type IntHeap []int
-
-func (h *IntHeap) Len() int           { return len(*h) }
-func (h *IntHeap) Less(i, j int) bool { return (*h)[i] > (*h)[j] }
-func (h *IntHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
-
-func (h *IntHeap) Push(x any) {
-	val, ok := x.(int)
-	if !ok {
-		return
-	}
-
-	*h = append(*h, val)
-}
-
-func (h *IntHeap) Pop() any {
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
-
-	return x
-}
 
 func checkLimits(value int, minLimit int, maxLimit int) bool {
 	if value >= minLimit && value <= maxLimit {
@@ -54,8 +31,8 @@ func main() {
 		return
 	}
 
-	markHeap := &IntHeap{}
-	*markHeap = make(IntHeap, 0, numberOfDishes)
+	markHeap := &myHeap.IntHeap{}
+	*markHeap = make(myHeap.IntHeap, 0, numberOfDishes)
 	heap.Init(markHeap)
 
 	for range numberOfDishes {
