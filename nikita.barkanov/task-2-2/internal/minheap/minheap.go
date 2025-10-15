ebed12596e00e8c60c6cc97157903321e6093123
m4ytaRo <maytarosheejsh@gmail.com>
2025-10-15 18:39:00 +0300

    [TASK-2-2] lint fix#1

--- nikita.barkanov/task-2-2/internal/minheap/minheap.go
+++ nikita.barkanov/task-2-2/internal/minheap/minheap.go
@@ -2,15 +2,16 @@ package minheap
 
 type IntHeap []int
 
-func (h IntHeap) Len() int           { return len(h) }
-func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
-func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h *IntHeap) Len() int           { return len(*h) }
+func (h *IntHeap) Less(i, j int) bool { return (*h)[i] < (*h)[j] }
+func (h *IntHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
 
 func (h *IntHeap) Push(x any) {
 	value, ok := x.(int)
 	if !ok {
 		panic("IntHeap.Push: expected int")
 	}
+
 	*h = append(*h, value)
 }
 
@@ -21,5 +22,6 @@ func (h *IntHeap) Pop() any {
 	n := len(old)
 	x := old[n-1]
 	*h = old[0 : n-1]
+
 	return x
 }
