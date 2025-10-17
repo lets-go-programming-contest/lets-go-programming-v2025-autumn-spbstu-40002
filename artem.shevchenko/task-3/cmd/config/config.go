01567946a452d89e5054c1be3782b1e553f55a7f
slendycs <slendycs@yandex.ru>
2025-10-17 17:14:22 +0300

    [TASK-3] Initial commit

---
+++ artem.shevchenko/task-3/cmd/config/config.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"gopkg.in/yaml.v3"
+	"os"
+
+	merr "github.com/slendycs/go-lab-3/cmd/myerrors"
+)
+
+type Config struct {
+	InputFile  string `yaml:"input-file"`
+	OutputFile string `yaml:"output-file"`
+}
+
+func ReadConfig(path string, config *Config) {
+	// Read data from file.
+	data, err := os.ReadFile(path)
+	if err != nil {
+		panic(merr.ErrNoConfigFileFound)
+	}
+
+	// Deserialize data from config file
+	err = yaml.Unmarshal(data, config)
+	if err != nil {
+		panic(merr.ErrFailedToDeserializeConfig)
+	}
+}
---
+++ artem.shevchenko/task-3/cmd/utils/json/json_writer.go
@@ -0,0 +1,49 @@
+package json
+
+import (
+	"encoding/json"
+	"os"
+
+	merr "github.com/slendycs/go-lab-3/cmd/myerrors"
+	"github.com/slendycs/go-lab-3/cmd/utils/xml"
+)
+
+type JsonData struct {
+	NumCode  string `json:"num_code"`
+	CharCode string `json:"char_code"`
+	Value    string `json:"value"`
+}
+
+func MakeJsonFromData(path string, data *xml.ValCurs) {
+	// Try to open output file.
+	file, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		panic(merr.ErrFailedToOpenOutputFile)
+	}
+	defer file.Close()
+
+	// Create a slice of valute data.
+	output := make([]JsonData, 0)
+	for _, valute := range data.Valute {
+		// Create a JSON record of data.
+		outputData := JsonData{
+			NumCode:  valute.NumCode,
+			CharCode: valute.CharCode,
+			Value:    valute.Value,
+		}
+
+		output = append(output, outputData)
+	}
+
+	// Serialize data
+	rawData, err := json.MarshalIndent(output, "", "  ")
+	if err != nil {
+		panic(merr.ErrFailedToSerializeJSON)
+	}
+
+	// Write data
+	_, err = file.Write(rawData)
+	if err != nil {
+		panic(merr.ErrFailedToWriteData)
+	}
+}
