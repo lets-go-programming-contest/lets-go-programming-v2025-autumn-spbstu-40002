d18d2a918eaf9f8fc609da5b7dee667052fdfd85
slendycs <slendycs@yandex.ru>
2025-10-18 00:22:07 +0300

    [TASK-3] The data type in the output JSON has been changed

--- artem.shevchenko/task-3/internal/json/json_writer.go
+++ artem.shevchenko/task-3/internal/json/json_writer.go
@@ -2,20 +2,52 @@ package json
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 	"path/filepath"
+	"strconv"
 
 	merr "github.com/slendycs/go-lab-3/internal/myerrors"
 	"github.com/slendycs/go-lab-3/internal/xml"
 )
 
 type JsonData struct {
-	NumCode  string `json:"num_code"`
+	NumCode  int `json:"num_code"`
 	CharCode string `json:"char_code"`
-	Value    string `json:"value"`
+	Value    float64 `json:"value"`
 }
 
 func MakeJsonFromData(path string, data *xml.ValCurs) {
+	// Create a slice of valute data.
+	output := make([]JsonData, 0)
+	for _, valute := range data.Valute {
+		// Create a JSON record of data.
+		intNumCode, err := strconv.Atoi(valute.NumCode)
+		if err != nil {
+			fmt.Println(merr.ErrNumCodeIsNotIneger)
+			return
+		}
+		floatValue, err := strconv.ParseFloat(valute.Value, 64)
+		if err != nil {
+			fmt.Println(merr.ErrValueIsNotFloat)
+			return
+		}
+
+		outputData := JsonData{
+			NumCode:  intNumCode,
+			CharCode: valute.CharCode,
+			Value:    floatValue,
+		}
+
+		output = append(output, outputData)
+	}
+
+	// Serialize data
+	rawData, err := json.MarshalIndent(output, "", "  ")
+	if err != nil {
+		panic(merr.ErrFailedToSerializeJSON)
+	}
+
 	// Creating output directory
 	dir := filepath.Dir(path)
 	if err := os.MkdirAll(dir, 0755); err != nil {
@@ -28,25 +60,6 @@ func MakeJsonFromData(path string, data *xml.ValCurs) {
 		panic(merr.ErrFailedToOpenOutputFile)
 	}
 	defer file.Close()
-
-	// Create a slice of valute data.
-	output := make([]JsonData, 0)
-	for _, valute := range data.Valute {
-		// Create a JSON record of data.
-		outputData := JsonData{
-			NumCode:  valute.NumCode,
-			CharCode: valute.CharCode,
-			Value:    valute.Value,
-		}
-
-		output = append(output, outputData)
-	}
-
-	// Serialize data
-	rawData, err := json.MarshalIndent(output, "", "  ")
-	if err != nil {
-		panic(merr.ErrFailedToSerializeJSON)
-	}
 
 	// Write data
 	_, err = file.Write(rawData)
