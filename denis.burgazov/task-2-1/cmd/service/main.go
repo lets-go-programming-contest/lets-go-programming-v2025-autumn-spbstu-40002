f553ac5d765f35f9391a5087d993c35dabab3bfd
denis <burgazov@inbox.ru>
2025-10-17 22:29:45 +0300

    [TASK-2-1] Initial commit

---
+++ denis.burgazov/task-2-1/cmd/service/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+)
+
+func parse(a any, errText string) bool {
+	_, err := fmt.Scanln(a)
+	if err != nil {
+		fmt.Println(errText)
+		var errBuf string
+		_, _ = fmt.Scanln(&errBuf)
+	}
+	return err == nil
+}
+
+func main() {
+	var (
+		numberOfDept int
+		numberOfEmp  int
+	)
+
+	if !parse(&numberOfDept, "Invalid number of department") {
+		return
+	}
+
+	for i := 0; i < numberOfDept; i++ {
+		if !parse(&numberOfEmp, "Invalid number of employee") {
+			return
+		}
+
+		var (
+			minTemp  = 15
+			maxTemp  = 30
+			sign     string
+			currTemp int
+		)
+
+		for j := 0; j < numberOfEmp; j++ {
+			_, _ = fmt.Scan(&sign)
+
+			if !parse(&currTemp, "Invalid temperature value") {
+				return
+			}
+
+			if minTemp == -1 {
+				fmt.Println(-1)
+				continue
+			}
+
+			switch sign {
+			case ">=":
+				if currTemp <= maxTemp {
+					minTemp = max(currTemp, minTemp)
+				} else {
+					minTemp = -1
+				}
+			case "<=":
+				if currTemp >= minTemp {
+					maxTemp = min(currTemp, maxTemp)
+				} else {
+					minTemp = -1
+				}
+			default:
+				fmt.Println("Invalid input")
+				return
+			}
+
+			fmt.Println(minTemp)
+		}
+	}
+
+}
