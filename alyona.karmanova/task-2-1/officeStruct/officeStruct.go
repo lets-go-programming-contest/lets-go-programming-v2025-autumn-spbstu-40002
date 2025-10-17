b4b2939a30cd8c6744a558e9a5058fbc68cb3bd9
Karmanova Alyona <karaliona2006@gmail.com>
2025-10-17 10:46:10 +0300

    [TASK-2-1] add description error

--- alyona.karmanova/task-2-1/officeStruct/officeStruct.go
+++ alyona.karmanova/task-2-1/officeStruct/officeStruct.go
@@ -1,65 +1,65 @@
 package officeStruct
 
 type Office struct {
-	СurrentMax  int
-	СurrentMin  int
-	СurrentTemp int
+	CurrentMax  int
+	CurrentMin  int
+	CurrentTemp int
 }
 
 func (o *Office) GetCurrentTemp() int {
-	return o.СurrentTemp
+	return o.CurrentTemp
 }
 
 func (o *Office) ApplyLowerBound(desiredTemp int) {
-	if o.СurrentMin > o.СurrentMax {
-		o.СurrentTemp = -1
+	if o.CurrentMin > o.CurrentMax {
+		o.CurrentTemp = -1
 
 		return
 	}
 
-	if desiredTemp > o.СurrentMax {
-		o.СurrentMin = desiredTemp
-		o.СurrentTemp = -1
+	if desiredTemp > o.CurrentMax {
+		o.CurrentMin = desiredTemp
+		o.CurrentTemp = -1
 
 		return
 	}
 
-	if desiredTemp > o.СurrentMin {
-		o.СurrentMin = desiredTemp
+	if desiredTemp > o.CurrentMin {
+		o.CurrentMin = desiredTemp
 
-		if o.СurrentTemp < desiredTemp {
-			o.СurrentTemp = desiredTemp
+		if o.CurrentTemp < desiredTemp {
+			o.CurrentTemp = desiredTemp
 		}
 	}
 
-	if o.СurrentTemp == -1 {
-		o.СurrentTemp = -1
+	if o.CurrentTemp == -1 {
+		o.CurrentTemp = -1
 	}
 }
 
 func (o *Office) ApplyUpperBound(desiredTemp int) {
-	if o.СurrentMin > o.СurrentMax {
-		o.СurrentTemp = -1
+	if o.CurrentMin > o.CurrentMax {
+		o.CurrentTemp = -1
 
 		return
 	}
 
-	if desiredTemp < o.СurrentMin {
-		o.СurrentMax = desiredTemp
-		o.СurrentTemp = -1
+	if desiredTemp < o.CurrentMin {
+		o.CurrentMax = desiredTemp
+		o.CurrentTemp = -1
 
 		return
 	}
 
-	if desiredTemp < o.СurrentMax {
-		o.СurrentMax = desiredTemp
+	if desiredTemp < o.CurrentMax {
+		o.CurrentMax = desiredTemp
 
-		if o.СurrentTemp > desiredTemp {
-			o.СurrentTemp = desiredTemp
+		if o.CurrentTemp > desiredTemp {
+			o.CurrentTemp = desiredTemp
 		}
 	}
 
-	if o.СurrentTemp == -1 {
-		o.СurrentTemp = -1
+	if o.CurrentTemp == -1 {
+		o.CurrentTemp = -1
 	}
 }
