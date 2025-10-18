a86e1586c3628f902de1401cc0f51f8dd87223d1
Tsahaev <alishkatsamhaev@gmail.com>
2025-10-18 12:57:32 +0300

    [TASK-2-1] Fix code

--- tsahaev.ali/task-2-1/cmd/service/main.go
+++ tsahaev.ali/task-2-1/cmd/service/main.go
@@ -13,57 +13,52 @@ type Employee struct {
 	maxTemp int
 }
 
+const partsExpected = 2
+
 func readInt(scanner *bufio.Scanner) (int, error) {
 	scanner.Scan()
+
 	return strconv.Atoi(strings.TrimSpace(scanner.Text()))
 }
 
-func main() {
-	scanner := bufio.NewScanner(os.Stdin)
+func readEmployees(scanner *bufio.Scanner, count int) []*Employee {
+	employees := make([]*Employee, count)
 
-	departmentsCount, err := readInt(scanner)
-	if err != nil || departmentsCount <= 0 {
-		fmt.Fprintln(os.Stderr, "Ошибка чтения количества департаментов")
-		return
-	}
-
-	employeesCount, err := readInt(scanner)
-	if err != nil || employeesCount <= 0 {
-		fmt.Fprintln(os.Stderr, "Ошибка чтения количества сотрудников")
-		return
-	}
-
-	employees := make([]*Employee, employeesCount)
-	for i := range employees {
+	for empIndex := range employees {
 		scanner.Scan()
 		line := strings.TrimSpace(scanner.Text())
+
 		if line == "" {
 			continue
 		}
 
 		parts := strings.Fields(line)
-		if len(parts) != 2 {
+		if len(parts) != partsExpected {
 			continue
 		}
 
-		value, convErr := strconv.Atoi(parts[1])
-		if convErr != nil {
+		value, err := strconv.Atoi(parts[1])
+		if err != nil {
 			continue
 		}
 
-		employees[i] = &Employee{}
+		employees[empIndex] = &Employee{minTemp: 0, maxTemp: 0}
 
 		switch parts[0] {
 		case ">=":
-			employees[i].minTemp = value
+			employees[empIndex].minTemp = value
 		case "<=":
-			employees[i].maxTemp = value
+			employees[empIndex].maxTemp = value
 		default:
 			continue
 		}
 	}
 
-	for range make([]struct{}, departmentsCount) {
+	return employees
+}
+
+func processDepartments(deptCount int, employees []*Employee) {
+	for range make([]struct{}, deptCount) {
 		commonMinTemp := 15
 		commonMaxTemp := 30
 
@@ -71,9 +66,11 @@ func main() {
 			if emp == nil {
 				continue
 			}
+
 			if emp.minTemp > commonMinTemp {
 				commonMinTemp = emp.minTemp
 			}
+
 			if emp.maxTemp < commonMaxTemp {
 				commonMaxTemp = emp.maxTemp
 			}
@@ -85,4 +82,26 @@ func main() {
 			fmt.Println(-1)
 		}
 	}
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	departmentsCount, err := readInt(scanner)
+	if err != nil || departmentsCount <= 0 {
+		fmt.Fprintln(os.Stderr, "Ошибка чтения количества департаментов")
+
+		return
+	}
+
+	employeesCount, err := readInt(scanner)
+	if err != nil || employeesCount <= 0 {
+		fmt.Fprintln(os.Stderr, "Ошибка чтения количества сотрудников")
+
+		return
+	}
+
+	employees := readEmployees(scanner, employeesCount)
+
+	processDepartments(departmentsCount, employees)
 }
