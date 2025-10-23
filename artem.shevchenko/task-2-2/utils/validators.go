2e7a07f0d4c30df3f8c1a9e88f1a927f79cfe143
slendycs <slendycs@yandex.ru>
2025-10-24 00:49:10 +0300

    [TASK-2-2] Added error return for validators and panic for incorrect element in heap

--- artem.shevchenko/task-2-2/utils/validators.go
+++ artem.shevchenko/task-2-2/utils/validators.go
@@ -7,41 +7,35 @@ import (
 	"github.com/slendycs/task-2-2/myerrors"
 )
 
-func ReadDishesCount() (int, bool) {
+func ReadDishesCount() (int, error) {
 	var dishCount int
 
 	_, err := fmt.Scan(&dishCount)
 	if err != nil || !(constants.MinDishCount <= dishCount && dishCount <= constants.MaxDishCount) {
-		fmt.Println(myerrors.ErrIncorrectDishesCount)
-
-		return 0, false
+		return 0, myerrors.ErrIncorrectDishesCount
 	}
 
-	return dishCount, true
+	return dishCount, nil
 }
 
-func ReadDishesRaiting() (int, bool) {
+func ReadDishesRaiting() (int, error) {
 	var dishRaiting int
 
 	_, err := fmt.Scan(&dishRaiting)
 	if err != nil || !(constants.MinRaiting <= dishRaiting && dishRaiting <= constants.MaxRaiting) {
-		fmt.Println(myerrors.ErrIncorrectRaiting)
-
-		return 0, false
+		return 0, myerrors.ErrIncorrectRaiting
 	}
 
-	return dishRaiting, true
+	return dishRaiting, nil
 }
 
-func ReadPickedDish(dishCount int) (int, bool) {
+func ReadPickedDish(dishCount int) (int, error) {
 	var pickedDish int
 
 	_, err := fmt.Scan(&pickedDish)
 	if err != nil || !(1 <= pickedDish && pickedDish <= dishCount) {
-		fmt.Println(myerrors.ErrIncorrectPickedDish)
-
-		return 0, false
+		return 0, myerrors.ErrIncorrectPickedDish
 	}
 
-	return pickedDish, true
+	return pickedDish, nil
 }
