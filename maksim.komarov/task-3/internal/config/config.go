198facd962ae39854a22eccc3a30343791b96ee9
MeguruMacabre <megurumacabre@gmail.com>
2025-10-24 19:08:03 +0300

    [TASK-3] fix lint 3

--- maksim.komarov/task-3/internal/config/config.go
+++ maksim.komarov/task-3/internal/config/config.go
@@ -23,7 +23,7 @@ func Load(path string) (AppConfig, error) {
 
 	file, err := os.Open(path)
 	if err != nil {
-		return AppConfig{}, fmt.Errorf("%s: %w", ErrOpenConfig, err)
+		return AppConfig{}, fmt.Errorf("%s: %w", ErrOpenConfig.Error(), err)
 	}
 
 	defer func() {
@@ -33,7 +33,7 @@ func Load(path string) (AppConfig, error) {
 	decoder := yaml.NewDecoder(file)
 
 	if err := decoder.Decode(&cfg); err != nil {
-		return AppConfig{}, fmt.Errorf("%s: %w", ErrDecodeConfig, err)
+		return AppConfig{}, fmt.Errorf("%s: %w", ErrOpenConfig.Error(), err)
 	}
 
 	return cfg, nil
