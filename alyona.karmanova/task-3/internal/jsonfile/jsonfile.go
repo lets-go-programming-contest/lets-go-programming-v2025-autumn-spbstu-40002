b68fe078ddb4fd551c3100fda165477f312d4ee7
Karmanova Alyona <karaliona2006@gmail.com>
2025-10-24 14:17:20 +0300

    [TASK-3] fix build

--- alyona.karmanova/task-3/internal/jsonfile/jsonfile.go
+++ alyona.karmanova/task-3/internal/jsonfile/jsonfile.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	"os"
 
-	outputfile "github.com/HuaChenju/task-3/internal/outputfile"
+	outputfile "github.com/HuaChenju/task-3/internal/jsonfile/outputfile"
 	xmlmodel "github.com/HuaChenju/task-3/internal/xmlfile/model"
 	"gopkg.in/yaml.v3"
 )
