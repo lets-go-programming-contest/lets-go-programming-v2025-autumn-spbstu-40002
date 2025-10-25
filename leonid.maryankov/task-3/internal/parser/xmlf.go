5f847084ab6594bc64bdaa73a609d5b948c7717e
Leon-Maryankov <mariannnkovleonid@gmail.com>
2025-10-25 22:08:46 +0300

    [TASK-3] added encoding processing and changed errors

--- leonid.maryankov/task-3/internal/parser/xmlf.go
+++ leonid.maryankov/task-3/internal/parser/xmlf.go
@@ -1,10 +1,14 @@
 package parser
 
 import (
+	"bytes"
 	"encoding/xml"
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
+
+	"golang.org/x/net/html/charset"
 )
 
 type ValCurs struct {
@@ -12,8 +16,10 @@ type ValCurs struct {
 }
 
 type Valute struct {
+	ID       string  `xml:"ID,attr"`
 	NumCode  int     `xml:"NumCode"`
 	CharCode string  `xml:"CharCode"`
+	Nominal  int     `xml:"Nominal"`
 	ValueStr string  `xml:"Value"`
 	Value    float64 `xml:"-"`
 }
@@ -21,28 +27,37 @@ type Valute struct {
 func ParseXML(path string) ([]Valute, error) {
 	data, err := os.ReadFile(path)
 	if err != nil {
-		return nil, fmt.Errorf("XML reading error: " + path + err.Error())
+		return nil, fmt.Errorf("xml reading error: %s: %w", path, err)
 	}
 
+	decoder := xml.NewDecoder(bytes.NewReader(data))
+	decoder.CharsetReader = charset.NewReaderLabel
+
 	var curs ValCurs
-	if err := xml.Unmarshal(data, &curs); err != nil {
-		return nil, fmt.Errorf("XML parsing error: " + path + err.Error())
+	if err := decoder.Decode(&curs); err != nil {
+		return nil, fmt.Errorf("xml parsing error: %s: %w", path, err)
 	}
 
 	for i := range curs.Valutes {
 		v := &curs.Valutes[i]
-		v.ValueStr = replaceComma(v.ValueStr)
-		v.Value, _ = strconv.ParseFloat(v.ValueStr, 64)
+
+		s := strings.TrimSpace(strings.ReplaceAll(v.ValueStr, ",", "."))
+		if s == "" {
+			return nil, fmt.Errorf("empty value for currency %s", v.CharCode)
+		}
+
+		f, err := strconv.ParseFloat(s, 64)
+		if err != nil {
+			return nil, fmt.Errorf("value parsing error for %s: %w", v.CharCode, err)
+		}
+
+		denom := 1
+		if v.Nominal > 0 {
+			denom = v.Nominal
+		}
+
+		v.Value = f / float64(denom)
 	}
 
 	return curs.Valutes, nil
-}
-
-func replaceComma(s string) string {
-	for i := range s {
-		if s[i] == ',' {
-			s = s[:i] + "." + s[i+1:]
-		}
-	}
-	return s
 }
