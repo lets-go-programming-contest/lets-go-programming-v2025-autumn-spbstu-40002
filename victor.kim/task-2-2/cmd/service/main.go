69df748e58143c3cdfe5a862d33127f5e7d9a1da
л1ьмщ <kimvo1511@gmail.com>
2025-10-26 22:13:50 +0300

    [TASK-2-2] Initial commit for task-2 main fix

--- victor.kim/task-2-2/cmd/service/main.go
+++ victor.kim/task-2-2/cmd/service/main.go
@@ -2,42 +2,52 @@ package main
 
 import (
 	"container/heap"
+	"errors"
 	"fmt"
 
 	"github.com/victor.kim/task-2-2/heaputils"
 )
 
+var (
+	errInvalidTotal = errors.New("invalid number of elements")
+	errInvalidValue = errors.New("invalid input value")
+	errInvalidK     = errors.New("invalid k value")
+)
+
 func main() {
 	var total int
 	if _, err := fmt.Scan(&total); err != nil || total <= 0 {
+		fmt.Println(errInvalidTotal)
 		return
 	}
 
 	arr := make([]int, total)
 	for i := range arr {
 		if _, err := fmt.Scan(&arr[i]); err != nil {
+			fmt.Println(errInvalidValue)
 			return
 		}
 	}
 
 	var kth int
 	if _, err := fmt.Scan(&kth); err != nil || kth <= 0 || kth > total {
+		fmt.Println(errInvalidK)
 		return
 	}
 
-	heapInst := &heaputils.IntHeap{}
-	heap.Init(heapInst)
+	h := &heaputils.IntHeap{}
+	heap.Init(h)
 
 	for i := range arr[:kth] {
-		heap.Push(heapInst, arr[i])
+		heap.Push(h, arr[i])
 	}
 
 	for _, v := range arr[kth:] {
-		if v > (*heapInst)[0] {
-			heap.Pop(heapInst)
-			heap.Push(heapInst, v)
+		if v > (*h)[0] {
+			heap.Pop(h)
+			heap.Push(h, v)
 		}
 	}
 
-	fmt.Println((*heapInst)[0])
+	fmt.Println((*h)[0])
 }
