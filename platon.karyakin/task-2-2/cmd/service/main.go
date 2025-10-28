09a5cd9748d83756d15a12c1946d0ce8efc01faa
Платон Карякин <viceone.von@gmail.com>
2025-10-28 12:57:55 +0300

    [TASK-2-2] update main.go

--- platon.karyakin/task-2-2/cmd/service/main.go
+++ platon.karyakin/task-2-2/cmd/service/main.go
@@ -11,6 +11,8 @@ import (
 func main() {
 	var itemCount int
 	if _, err := fmt.Fscan(os.Stdin, &itemCount); err != nil {
+		fmt.Fprintln(os.Stderr, ktherr.ErrReadItemCount)
+		
 		return
 	}
 
@@ -23,25 +25,46 @@ func main() {
 	values := make([]int, itemCount)
 	for i := range values {
 		if _, err := fmt.Fscan(os.Stdin, &values[i]); err != nil {
+			fmt.Fprintln(os.Stderr, ktherr.ErrReadValue)
+			
 			return
 		}
 
 		if values[i] < -10000 || values[i] > 10000 {
+			fmt.Fprintln(os.Stderr, ktherr.ErrValueOutOfRange)
+			
 			return
 		}
 	}
 
 	var position int
 	if _, err := fmt.Fscan(os.Stdin, &position); err != nil {
+		fmt.Fprintln(os.Stderr, ktherr.ErrReadPosition)
+		
 		return
 	}
 
 	if position < 1 || position > itemCount {
+		fmt.Fprintln(os.Stderr, ktherr.ErrPositionOutOfRange)
+		
 		return
 	}
 
 	result, err := kth.KthMostPreferred(values, position)
 	if err != nil {
+		if errors.Is(err, ktherr.ErrEmptyResult) {
+			fmt.Fprintln(os.Stderr, ktherr.ErrEmptyResult)
+			
+			return
+		}
+		if errors.Is(err, ktherr.ErrPositionOutOfRange) {
+			fmt.Fprintln(os.Stderr, ktherr.ErrPositionOutOfRange)
+			
+			return
+		}
+
+		fmt.Fprintln(os.Stderr, err)
+		
 		return
 	}
 
