6cb41a618334c1427b878476c186b18209110595
t1wt07 <t1wt.work@gmail.com>
2025-10-31 00:09:18 +0300

    [TASK-2-2] big fix

--- timur.usachev/task-2-2/go.mod
+++ timur.usachev/task-2-2/go.mod
@@ -1,3 +1,3 @@
-module github.com/t1wt/task1
+module github.com/t1wt/task-2-2
 
 go 1.22.7
---
+++ timur.usachev/task-2-2/internal/IntHeap/IntHeap.go
@@ -0,0 +1,63 @@
+package IntHeap
+
+import "container/heap"
+
+type IntHeap struct {
+	data []int
+}
+
+func (h *IntHeap) Len() int {
+	return len(h.data)
+}
+
+func (h *IntHeap) Less(i, j int) bool {
+	return h.data[i] < h.data[j]
+}
+
+func (h *IntHeap) Swap(i, j int) {
+	h.data[i], h.data[j] = h.data[j], h.data[i]
+}
+
+func (h *IntHeap) Push(x interface{}) {
+	v, ok := x.(int)
+
+	if !ok {
+		panic("IntHeap.Push: value is not int")
+	}
+
+	h.data = append(h.data, v)
+}
+
+func (h *IntHeap) Pop() interface{} {
+	if len(h.data) == 0 {
+		panic("IntHeap.Pop: empty IntHeap")
+	}
+
+	n := len(h.data)
+	x := h.data[n-1]
+	h.data = h.data[:n-1]
+
+	return x
+}
+
+func (h *IntHeap) Peek() int {
+	if len(h.data) == 0 {
+		panic("empty")
+	}
+	return h.data[0]
+}
+
+func FindKthLargest(nums []int, k int) int {
+	h := &IntHeap{}
+	heap.Init(h)
+
+	for _, num := range nums {
+		heap.Push(h, num)
+
+		if h.Len() > k {
+			heap.Pop(h)
+		}
+	}
+
+	return h.Peek()
+}
