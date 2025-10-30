f3457dec6f23810633a212a81d36af2ec78a99d5
t1wt07 <t1wt.work@gmail.com>
2025-10-31 00:09:18 +0300

    [TASK-2-2] big fix

--- timur.usachev/task-2-2/cmd/service/main.go
+++ timur.usachev/task-2-2/cmd/service/main.go
@@ -1,66 +1,37 @@
 package main
 
 import (
-	"container/heap"
+	"errors"
 	"fmt"
 	"os"
+
+	"github.com/t1wt/task-2-2/internal/IntHeap"
 )
 
-type IntHeap struct {
-	data []int
-}
-
-func (h *IntHeap) Len() int {
-	return len(h.data)
-}
-
-func (h *IntHeap) Less(i, j int) bool {
-	return h.data[i] < h.data[j]
-}
-
-func (h *IntHeap) Swap(i, j int) {
-	h.data[i], h.data[j] = h.data[j], h.data[i]
-}
-
-func (h *IntHeap) Push(x interface{}) {
-	v, ok := x.(int)
-	if !ok {
-		return
-	}
-
-	h.data = append(h.data, v)
-}
-
-func (h *IntHeap) Pop() interface{} {
-	if len(h.data) == 0 {
-		return nil
-	}
-
-	n := len(h.data)
-	x := h.data[n-1]
-	h.data = h.data[:n-1]
-
-	return x
-}
+var (
+	errInvalidCount     = errors.New("invalid input: count must be a positive int")
+	errFailedReadValues = errors.New("invalid input: failed to read values :(")
+	errInvalidKth       = errors.New("invalid input: 1 <= k <= count")
+)
 
 func readInput() ([]int, int) {
 	var count int
 	if _, err := fmt.Fscan(os.Stdin, &count); err != nil || count <= 0 {
-		fmt.Fprintln(os.Stderr, "invalid input: count must be a positive integer")
+		fmt.Fprintln(os.Stderr, errInvalidCount)
 		os.Exit(1)
 	}
 
 	values := make([]int, count)
-	for i := range make([]struct{}, count) {
+	for i := 0; i < count; i++ {
 		if _, err := fmt.Fscan(os.Stdin, &values[i]); err != nil {
-			fmt.Fprintln(os.Stderr, "invalid input: failed to read values")
+			fmt.Fprintln(os.Stderr, errFailedReadValues)
 			os.Exit(1)
 		}
 	}
 
 	var kth int
 	if _, err := fmt.Fscan(os.Stdin, &kth); err != nil || kth <= 0 || kth > count {
-		fmt.Fprintln(os.Stderr, "invalid input: k must be between 1 and count")
+		fmt.Fprintln(os.Stderr, errInvalidKth)
 		os.Exit(1)
 	}
 
@@ -70,31 +41,6 @@ func readInput() ([]int, int) {
 func main() {
 	values, kth := readInput()
 
-	minHeap := &IntHeap{data: []int{}}
-	heap.Init(minHeap)
-
-	for i := range make([]struct{}, kth) {
-		heap.Push(minHeap, values[i])
-	}
-
-	for j := range make([]struct{}, len(values)-kth) {
-		val := values[kth+j]
-
-		if minHeap.Len() == 0 {
-			continue
-		}
-
-		if val > minHeap.data[0] {
-			heap.Pop(minHeap)
-			heap.Push(minHeap, val)
-		}
-	}
-
-	if minHeap.Len() == 0 {
-		fmt.Fprintln(os.Stderr, "no result")
-		os.Exit(1)
-	}
-
-	top := minHeap.data[0]
-	fmt.Println(top)
+	result := IntHeap.FindKthLargest(values, kth)
+	fmt.Println(result)
 }
