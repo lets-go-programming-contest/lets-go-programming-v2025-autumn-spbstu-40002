2fd3ac0d4ed79a390fdb13076db9c133e05514f8
Yaroslave135 <rickmargo135@gmail.com>
2025-10-30 14:29:56 +0300

    [TASK-1]

--- yaroslav.petrauskas/task-1/cmd/service/main.go
+++ yaroslav.petrauskas/task-1/cmd/service/main.go
@@ -3,25 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	var a, b int
-	var op string
-
-	fmt.Scan(&a, &b, &op)
-
-	switch op {
-	case "+":
-		fmt.Println(a + b)
-	case "-":
-		fmt.Println(a - b)
-	case "*":
-		fmt.Println(a * b)
-	case "/":
-		if b == 0 {
-			fmt.Println("Division by zero")
-		} else {
-			fmt.Println(a / b)
-		}
-	default:
-		fmt.Println("Invalid operation")
-	}
+    var a, b int
+    var op string
+    fmt.Scan(&a, &b, &op)
+    
+    switch op {
+    case "+":
+        fmt.Println(a + b)
+    case "-":
+        fmt.Println(a - b)
+    case "*":
+        fmt.Println(a * b)
+    case "/":
+        if b == 0 {
+            fmt.Println("Division by zero")
+        } else {
+            fmt.Println(a / b)
+        }
+    default:
+        fmt.Println("Invalid operation")
+    }
 }
