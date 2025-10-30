69af43860b3f0f8277b631f1bbb9f87d69780e73
Сараев Гордей <isisu7337@gmail.com>
2025-10-31 00:52:58 +0300

    [TASK-3] fix lint x8

--- gordey.saraev/task-3/internal/currency/currency.go
+++ gordey.saraev/task-3/internal/currency/currency.go
@@ -22,11 +22,13 @@ type Currency struct {
 func ValuteToCurrency(valute Valute) *Currency {
 	cleanValue := strings.ReplaceAll(valute.Value, ",", ".")
 	value, err := strconv.ParseFloat(cleanValue, 64)
+
 	if err != nil {
 		value = 0
 	}
 
 	numCode, err := strconv.Atoi(valute.NumCode)
+
 	if err != nil {
 		numCode = 0
 	}
