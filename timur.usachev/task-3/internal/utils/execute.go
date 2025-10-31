9e00e63512066679a25458c9c95bc50531be7f10
t1wt07 <t1wt.work@gmail.com>
2025-10-31 19:21:03 +0300

    [TASK-3] revert: fix linter again

--- timur.usachev/task-3/internal/utils/execute.go
+++ timur.usachev/task-3/internal/utils/execute.go
@@ -13,7 +13,7 @@ func Execute(configPath string) error {
 
 	data, err := os.ReadFile(cfg.InputFile)
 	if err != nil {
-		return fmt.Errorf("%w", ErrXMLRead)
+		return fmt.Errorf("%v: %w", ErrXMLRead, err)
 	}
 
 	root, err := parseXML(data)
@@ -23,8 +23,7 @@ func Execute(configPath string) error {
 
 	out := convertValutes(root)
 
-	err = writeJSON(out, cfg.OutputFile)
-	if err != nil {
+	if err = writeJSON(out, cfg.OutputFile); err != nil {
 		return err
 	}
 
