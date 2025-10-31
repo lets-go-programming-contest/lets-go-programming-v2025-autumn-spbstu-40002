7a719688ab304a75c37657d4ec8e1238889e0c93
sonychello <sofapozneeva@gmail.com>
2025-10-31 20:48:23 +0300

    [TASK-3] Update json.go

--- sofya.pozneeva/task-3/internal/utils/json.go
+++ sofya.pozneeva/task-3/internal/utils/json.go
@@ -12,7 +12,7 @@ const (
 )
 
 type CurrencyOutput struct {
-	NumCode  int  `json:"num_code"`
+	NumCode  int     `json:"num_code"`
 	CharCode string  `json:"char_code"`
 	Value    float64 `json:"value"`
 }
