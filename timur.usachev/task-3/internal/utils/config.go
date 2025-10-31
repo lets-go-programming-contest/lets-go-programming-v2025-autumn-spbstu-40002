520ffdfd3e6f4a3ff14ffb29bcd08f515b7ca783
t1wt07 <t1wt.work@gmail.com>
2025-10-31 19:37:20 +0300

    [TASK-3] fix nlines and non-wrapping format

--- timur.usachev/task-3/internal/utils/config.go
+++ timur.usachev/task-3/internal/utils/config.go
@@ -14,13 +14,13 @@ func loadConfig(path string) (Config, error) {
 
 	data, err := os.ReadFile(path)
 	if err != nil {
-		return Config{}, fmt.Errorf("%v: %w", ErrConfigRead, err)
+		return Config{}, fmt.Errorf("%s: %w", ErrConfigRead.Error(), err)
 	}
 
 	var cfg Config
 
 	if err = yaml.Unmarshal(data, &cfg); err != nil {
-		return Config{}, fmt.Errorf("%v: %w", ErrConfigParse, err)
+		return Config{}, fmt.Errorf("%s: %w", ErrConfigParse.Error(), err)
 	}
 
 	return cfg, nil
