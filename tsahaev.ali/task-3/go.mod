e2604aec80e725e47d0bc9ca348fb3a3629b1da1
Tsahaev <alishkatsamhaev@gmail.com>
2025-11-09 19:26:39 +0300

    [TASK-3] Fix tests and lint

--- tsahaev.ali/task-3/go.mod
+++ tsahaev.ali/task-3/go.mod
@@ -1,8 +1,10 @@
-module task-3
+module github.com/Tsahaev/task-3
 
-go 1.22.7
+go 1.22
 
 require (
-    gopkg.in/yaml.v3 v3.0.1
-    golang.org/x/text v0.14.0
+	golang.org/x/net v0.35.0
+	gopkg.in/yaml.v3 v3.0.1
 )
+
+require golang.org/x/text v0.22.0 // indirect
---
+++ tsahaev.ali/task-3/internal/exporter/json.go
@@ -0,0 +1,34 @@
+package exporter
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+
+	"github.com/Tsahaev/task-3/internal/currency"
+	"github.com/Tsahaev/task-3/internal/myerrors"
+	"github.com/Tsahaev/task-3/internal/utils"
+)
+
+const dirPerm = 0o755
+
+func WriteToJSON(valutes []currency.Valute, path string) {
+	jsonValutes := utils.ParseValutesToJSON(valutes)
+
+	if err := os.MkdirAll(filepath.Dir(path), dirPerm); err != nil {
+		panic(myerrors.ErrDirCreate)
+	}
+
+	file, err := os.Create(path)
+	if err != nil {
+		panic(myerrors.ErrOutOpen)
+	}
+	defer file.Close()
+
+	enc := json.NewEncoder(file)
+	enc.SetIndent("", "  ")
+
+	if err := enc.Encode(jsonValutes); err != nil {
+		panic(myerrors.ErrOutEncode)
+	}
+}
