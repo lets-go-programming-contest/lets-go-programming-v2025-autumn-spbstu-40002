dbedb2059c8f00b62ed3306863294998eb1e4b07
Tsahaev <alishkatsamhaev@gmail.com>
2025-11-09 19:26:39 +0300

    [TASK-3] Fix tests and lint

--- tsahaev.ali/task-3/cmd/service/main.go
+++ tsahaev.ali/task-3/cmd/service/main.go
@@ -1,165 +1,17 @@
 package main
 
 import (
-	"encoding/json"
-	"encoding/xml"
-	"flag"
-	"fmt"
-	"io"
-	"os"
-	"path/filepath"
-	"sort"
-	"strings"
-
-	"golang.org/x/text/encoding/charmap"
-
-	"gopkg.in/yaml.v3"
+	"github.com/Tsahaev/task-3/internal/exporter"
+	"github.com/Tsahaev/task-3/internal/parser/config"
+	"github.com/Tsahaev/task-3/internal/parser/xmlparser"
 )
 
-type Config struct {
-	InputFile  string `yaml:"input-file"`
-	OutputFile string `yaml:"output-file"`
-}
-
-type ValCurs struct {
-	XMLName xml.Name `xml:"ValCurs"`
-	Valutes []Valute `xml:"Valute"`
-}
-
-type Valute struct {
-	XMLName  xml.Name `xml:"Valute"`
-	NumCode  string   `xml:"NumCode"`
-	CharCode string   `xml:"CharCode"`
-	Value    string   `xml:"Value"`
-}
-
-type CurrencyResult struct {
-	NumCode  string  `json:"num_code"`
-	CharCode string  `json:"char_code"`
-	Value    float64 `json:"value"`
-}
-
 func main() {
-	configPath := flag.String("config", "", "Path to configuration file")
-	flag.Parse()
+	confPath := config.ReadConfigPath()
 
-	if *configPath == "" {
-		panic("Config path is required")
-	}
+	conf := config.ParseConfig(confPath)
 
-	config, err := loadConfig(*configPath)
-	if err != nil {
-		panic(fmt.Sprintf("Error loading config: %v", err))
-	}
+	xmlData := xmlparser.ParseXML(conf.InputFile)
 
-	valCurs, err := parseXML(config.InputFile)
-	if err != nil {
-		panic(fmt.Sprintf("Error parsing XML: %v", err))
-	}
-
-	currencies, err := convertAndSortCurrencies(valCurs)
-	if err != nil {
-		panic(fmt.Sprintf("Error converting currencies: %v", err))
-	}
-
-	err = saveResults(config.OutputFile, currencies)
-	if err != nil {
-		panic(fmt.Sprintf("Error saving results: %v", err))
-	}
-
-	fmt.Println("Successfully processed currencies")
-}
-
-func loadConfig(configPath string) (*Config, error) {
-	file, err := os.Open(configPath)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	data, err := io.ReadAll(file)
-	if err != nil {
-		return nil, err
-	}
-
-	var config Config
-	err = yaml.Unmarshal(data, &config)
-	if err != nil {
-		return nil, err
-	}
-
-	return &config, nil
-}
-
-func parseXML(filePath string) (*ValCurs, error) {
-	file, err := os.Open(filePath)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	data, err := io.ReadAll(file)
-	if err != nil {
-		return nil, err
-	}
-
-	decoder := xml.NewDecoder(strings.NewReader(string(data)))
-	decoder.CharsetReader = func(charset string, input io.Reader) (io.Reader, error) {
-		if charset == "windows-1251" {
-			return charmap.Windows1251.NewDecoder().Reader(input), nil
-		}
-		return input, nil
-	}
-
-	var valCurs ValCurs
-	err = decoder.Decode(&valCurs)
-	if err != nil {
-		return nil, err
-	}
-
-	return &valCurs, nil
-}
-
-func convertAndSortCurrencies(valCurs *ValCurs) ([]CurrencyResult, error) {
-	var currencies []CurrencyResult
-
-	for _, valute := range valCurs.Valutes {
-		cleanedValue := strings.Replace(valute.Value, ",", ".", -1)
-		var value float64
-		_, err := fmt.Sscanf(cleanedValue, "%f", &value)
-		if err != nil {
-			continue
-		}
-
-		currencies = append(currencies, CurrencyResult{
-			NumCode:  valute.NumCode,
-			CharCode: valute.CharCode,
-			Value:    value,
-		})
-	}
-
-	sort.Slice(currencies, func(i, j int) bool {
-		return currencies[i].Value > currencies[j].Value
-	})
-
-	return currencies, nil
-}
-
-func saveResults(outputPath string, currencies []CurrencyResult) error {
-	dir := filepath.Dir(outputPath)
-	err := os.MkdirAll(dir, 0755)
-	if err != nil {
-		return err
-	}
-
-	file, err := os.Create(outputPath)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	encoder := json.NewEncoder(file)
-	encoder.SetIndent("", "    ")
-
-	return encoder.Encode(currencies)
+	exporter.WriteToJSON(xmlData.Valute, conf.OutputFile)
 }
