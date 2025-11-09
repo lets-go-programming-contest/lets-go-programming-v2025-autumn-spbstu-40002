4660dd5e95c53636273eda3367559865950834df
Tsahaev <alishkatsamhaev@gmail.com>
2025-11-09 19:30:32 +0300

    [TASK-3] Fix yaml errors attempt 2

--- tsahaev.ali/task-3/internal/currency/currency.go
+++ tsahaev.ali/task-3/internal/currency/currency.go
@@ -8,13 +8,13 @@ type ValCurs struct {
 }
 
 type Valute struct {
-	NumCode  string `xml:"NumCode"`
+	NumCode  int    `xml:"NumCode"`
 	CharCode string `xml:"CharCode"`
 	Value    string `xml:"Value"`
 }
 
 type JSONValute struct {
-	NumCode  string  `json:"num_code"`
+	NumCode  int     `json:"num_code"`
 	CharCode string  `json:"char_code"`
 	Value    float64 `json:"value"`
 }
--- tsahaev.ali/task-3/internal/utils/converter.go
+++ tsahaev.ali/task-3/internal/utils/converter.go
@@ -9,24 +9,29 @@ import (
 
 func ParseCurrencyValue(val string) float64 {
 	if val == "" {
-		return 0
+		return 0.0
 	}
+
 	val = strings.Replace(val, ",", ".", 1)
-	res, err := strconv.ParseFloat(val, 64)
+
+	floatVal, err := strconv.ParseFloat(val, 64)
 	if err != nil {
-		return 0
+		return 0.0
 	}
-	return res
+
+	return floatVal
 }
 
 func ParseValutesToJSON(valutes []currency.Valute) []currency.JSONValute {
-	out := make([]currency.JSONValute, 0, len(valutes))
-	for _, v := range valutes {
-		out = append(out, currency.JSONValute{
-			NumCode:  v.NumCode,
-			CharCode: v.CharCode,
-			Value:    ParseCurrencyValue(v.Value),
+	result := make([]currency.JSONValute, 0, len(valutes))
+
+	for _, cur := range valutes {
+		result = append(result, currency.JSONValute{
+			NumCode:  cur.NumCode,
+			CharCode: cur.CharCode,
+			Value:    ParseCurrencyValue(cur.Value),
 		})
 	}
-	return out
+
+	return result
 }
