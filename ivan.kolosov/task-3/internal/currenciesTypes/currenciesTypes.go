a0ae8bc329f4b3e6215185e9e9ca7e3d4f0442d4
InsomniaDemon <chelovefick@gmail.com>
2025-11-16 14:07:42 +0300

    [TASK-3] fiting with lint

--- ivan.kolosov/task-3/internal/currenciesTypes/currenciesTypes.go
+++ ivan.kolosov/task-3/internal/currenciesTypes/currenciesTypes.go
@@ -16,7 +16,7 @@ var (
 type Currency struct {
 	NumCode  int     `json:"num_code" xml:"NumCode"`
 	CharCode string  `json:"char_code" xml:"CharCode"`
-	Value    float64 `json:"value xml:"Value""`
+	Value    float64 `json:"value" xml:"Value"`
 }
 
 type Currencies struct {
