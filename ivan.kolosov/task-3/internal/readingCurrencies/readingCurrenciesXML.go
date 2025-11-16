e980f8731fd26f8ecc4c124b4e8f7d8faaed34c8
InsomniaDemon <chelovefick@gmail.com>
2025-11-16 13:57:37 +0300

    [TASK-3] uploading changes

--- ivan.kolosov/task-3/internal/readingCurrencies/readingCurrenciesXML.go
+++ ivan.kolosov/task-3/internal/readingCurrencies/readingCurrenciesXML.go
@@ -5,72 +5,24 @@ import (
 	"errors"
 	"fmt"
 	"os"
-	"strconv"
-	"strings"
 
 	"golang.org/x/net/html/charset"
+
+	cur "github.com/InsomniaDemon/task-3/internal/currenciesTypes"
 )
 
 var (
 	errParsingXML     = errors.New("error occurred while parsing xml file")
-	errParsingFloat   = errors.New("error occurred while parsing float")
 	errOpeningXMLFile = errors.New("error occurred while opening xml file")
 	errClosingXMLFile = errors.New("error occurred while closing xml file")
 )
 
-func (cur *CurrencyXML) UnmarshalXML(dec *xml.Decoder, start xml.StartElement) error {
-	var temp struct {
-		ID        string `xml:"ID,attr"`
-		NumCode   int    `xml:"NumCode"`
-		CharCode  string `xml:"CharCode"`
-		Nominal   int    `xml:"Nominal"`
-		Name      string `xml:"Name"`
-		Value     string `xml:"Value"`
-		VunitRate string `xml:"VunitRate"`
-	}
-
-	err := dec.DecodeElement(&temp, &start)
-	if err != nil {
-		return fmt.Errorf("%w: %w", errParsingXML, err)
-	}
-
-	str := strings.ReplaceAll(temp.Value, ",", ".")
-	if str == "" {
-		cur.ValueFloat = 0.0
-	} else {
-		cur.ValueFloat, err = strconv.ParseFloat(str, 64)
-		if err != nil {
-			return fmt.Errorf("%w: %w", errParsingFloat, err)
-		}
-	}
-
-	str = strings.ReplaceAll(temp.VunitRate, ",", ".")
-	if str == "" {
-		cur.VunitRateFloat = 0.0
-	} else {
-		cur.VunitRateFloat, err = strconv.ParseFloat(str, 64)
-		if err != nil {
-			return fmt.Errorf("%w: %w", errParsingFloat, err)
-		}
-	}
-
-	cur.ID = temp.ID
-	cur.NumCode = temp.NumCode
-	cur.CharCode = temp.CharCode
-	cur.Nominal = temp.Nominal
-	cur.Name = temp.Name
-	cur.Value = temp.Value
-	cur.VunitRate = temp.VunitRate
-
-	return nil
-}
-
-func GetCurrencies(path string) (_ CurrenciesXML, returnError error) {
-	var cur CurrenciesXML
+func GetCurrencies(path string) (_ cur.Currencies, returnError error) {
+	var currencies cur.Currencies
 
 	file, err := os.Open(path)
 	if err != nil {
-		return cur, fmt.Errorf("%w: %w", errOpeningXMLFile, err)
+		return currencies, fmt.Errorf("%w: %w", errOpeningXMLFile, err)
 	}
 
 	defer func() {
@@ -87,10 +39,10 @@ func GetCurrencies(path string) (_ CurrenciesXML, returnError error) {
 	dc := xml.NewDecoder(file)
 	dc.CharsetReader = charset.NewReaderLabel
 
-	err = dc.Decode(&cur)
+	err = dc.Decode(&currencies)
 	if err != nil {
-		return cur, fmt.Errorf("%w: %w", errParsingXML, err)
+		return currencies, fmt.Errorf("%w: %w", errParsingXML, err)
 	}
 
-	return cur, nil
+	return currencies, nil
 }
