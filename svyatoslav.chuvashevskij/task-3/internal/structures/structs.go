d16ac5ae58ae9296780c461b9262354afeb7413d
Chuvashevskij Svyatoslav <chuvashevskij2006@gmail.com>
2025-11-16 20:21:24 +0300

    [TASK-3] linter errors fixed

--- svyatoslav.chuvashevskij/task-3/internal/structures/structs.go
+++ svyatoslav.chuvashevskij/task-3/internal/structures/structs.go
@@ -7,32 +7,3 @@ type ValuteStruct struct {
 		Value    float64 `xml:"Value" json:"value"`
 	} `xml:"Valute" json:"valute"`
 }
-
-/*type ValuteStruct struct {
-	NumCode  int     `json:"num_code"`
-	CharCode string  `json:"char_code"`
-	Value    float64 `json:"value"`
-}*/
-
-/*type JSONStruct struct {
-	ValCurs []ValuteStruct `json:"valute"`
-}*/
-
-/*func ConvertXMLToJSON(xmlStruct ValuteStruct) (JSONStruct, error) {
-	jsonStruct := new(JSONStruct)
-
-	for _, valute := range xmlStruct.ValCurs {
-		floatValue, err := strconv.ParseFloat(strings.Replace(valute.Value, ",", ".", 1), 64)
-		if err != nil {
-			return JSONStruct{}, fmt.Errorf("invalid value: %w", err)
-		}
-
-		jsonStruct.ValCurs = append(jsonStruct.ValCurs, ValuteStruct{
-			NumCode:  valute.NumCode,
-			CharCode: valute.CharCode,
-			Value:    floatValue,
-		})
-	}
-
-	return *jsonStruct, nil
-}*/
