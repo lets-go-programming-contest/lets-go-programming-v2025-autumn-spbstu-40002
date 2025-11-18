75f5100edbb2cd735800fb1d9f3c33c62b27cba0
danila-clown <daphetad@gmail.com>
2025-11-18 07:48:08 +0300

    [TASK-5] war with linter part 2

--- danila.onitshuk/task-5/pkg/handlers/errorsAndConst.go
+++ danila.onitshuk/task-5/pkg/handlers/errorsAndConst.go
@@ -2,12 +2,12 @@ package handlers
 
 import "errors"
 
+var (
+	ErrNoDecorator = errors.New("can't be decorated")
+)
+
 const (
 	noDecoratorData        = "no decorator"
 	textForDecoratorString = "decorated: "
 	noMultiplexerData      = "no multiplexer"
-)
-
-var (
-	ErrNoDecorator = errors.New("can't be decorated")
 )
