d2292e387b26ecd79b009fca89e87f82e9026c75
Сараев Гордей <isisu7337@gmail.com>
2025-11-20 08:05:05 +0300

    [TASK-3] fix fix is broke

--- gordey.saraev/task-3/internal/currency/currency.go
+++ gordey.saraev/task-3/internal/currency/currency.go
@@ -2,30 +2,43 @@ package currency
 
 import (
 	"encoding/xml"
+	"fmt"
 	"strconv"
 	"strings"
 )
 
 type Currency struct {
-	XMLName  xml.Name `xml:"Valute"`
-	NumCode  string   `xml:"NumCode"  json:"num_code"`
+	XMLName  xml.Name `xml:"Valute"  json:"-"`
+	NumCode  int      `xml:"NumCode" json:"num_code"`
 	CharCode string   `xml:"CharCode" json:"char_code"`
-	Value    string   `xml:"Value"    json:"value"`
+	Value    float64  `xml:"Value"   json:"value"`
 }
 
-func (c *Currency) ToNumeric() (numCode int, value float64) {
-	num, err := strconv.Atoi(c.NumCode)
+func (c *Currency) UnmarshalXML(decoder *xml.Decoder, start xml.StartElement) error {
+	var raw struct {
+		NumCode  string `xml:"NumCode"`
+		CharCode string `xml:"CharCode"`
+		Value    string `xml:"Value"`
+	}
+
+	if err := decoder.DecodeElement(&raw, &start); err != nil {
+
+		return fmt.Errorf("failed to decode XML element: %w", err)
+	}
+
+	num, err := strconv.Atoi(raw.NumCode)
 	if err != nil {
 		num = 0
 	}
-	numCode = num
+	c.NumCode = num
 
-	cleanValue := strings.ReplaceAll(c.Value, ",", ".")
+	cleanValue := strings.ReplaceAll(raw.Value, ",", ".")
 	val, err := strconv.ParseFloat(cleanValue, 64)
 	if err != nil {
 		val = 0
 	}
-	value = val
+	c.Value = val
+	c.CharCode = raw.CharCode
 
-	return
+	return nil
 }
