66c75541b3fae3bb856fb09a81bed11799d094b0
t1wt <t1wt.work@gmail.com>
2025-11-25 01:23:18 +0300

    [TASK-5] fix main func + handlers issues for lint

--- timur.usachev/task-5/cmd/service/main.go
+++ timur.usachev/task-5/cmd/service/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"time"
 
@@ -10,28 +11,68 @@ import (
 )
 
 const (
-	chanSize       = 4
-	readIterations = 3
-	readTimeout    = 2 * time.Second
+	bufferSize     = 4
+	recvTimeoutSec = 2
+	repeatCount    = 3
 )
 
+func runConveyer(ctx context.Context, cvr *conveyer.Conveyer) <-chan error {
+	errCh := make(chan error, 1)
+
+	go func() {
+		errCh <- cvr.Run(ctx)
+	}()
+
+	return errCh
+}
+
+func sendInputs(cvr *conveyer.Conveyer, inputs []string) {
+	for _, item := range inputs {
+		if err := cvr.Send("input", item); err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func receiveOutputs(cvr *conveyer.Conveyer) {
+	for i := 0; i < repeatCount; i++ {
+		resCh := make(chan struct{})
+
+		var val string
+		var err error
+
+		go func() {
+			val, err = cvr.Recv("out")
+			close(resCh)
+		}()
+
+		select {
+		case <-resCh:
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+
+			fmt.Println("recv:", val)
+
+		case <-time.After(recvTimeoutSec * time.Second):
+			log.Println("timeout")
+		}
+	}
+}
+
 func main() {
-	cvr := conveyer.New(chanSize)
+	cvr := conveyer.New(bufferSize)
 
 	cvr.RegisterDecorator(handlers.PrefixDecoratorFunc, "input", "decorated")
 	cvr.RegisterSeparator(handlers.SeparatorFunc, "decorated", []string{"s1", "s2"})
 	cvr.RegisterMultiplexer(handlers.MultiplexerFunc, []string{"s1", "s2"}, "out")
 
 	ctx := context.Background()
-
 	ctxRun, cancelRun := context.WithCancel(ctx)
 	defer cancelRun()
 
-	runErrCh := make(chan error, 1)
-
-	go func() {
-		runErrCh <- cvr.Run(ctxRun)
-	}()
+	runErrCh := runConveyer(ctxRun, cvr)
 
 	inputs := []string{
 		"hello",
@@ -41,45 +82,15 @@ func main() {
 		"gopher",
 	}
 
-	for _, item := range inputs {
-		if err := cvr.Send("input", item); err != nil {
-			log.Println(err)
-		}
-	}
-
-	for range readIterations {
-		resCh := make(chan struct{})
-
-		var val string
-		var err error
-
-		go func() {
-			val, err = cvr.Recv("out")
-
-			close(resCh)
-		}()
-
-		select {
-		case <-resCh:
-			if err != nil {
-				log.Println(err)
-
-				continue
-			}
-
-			log.Println("recv:", val)
-
-		case <-time.After(readTimeout):
-			log.Println("timeout")
-		}
-	}
+	sendInputs(cvr, inputs)
+	receiveOutputs(cvr)
 
 	cancelRun()
 
 	if err := <-runErrCh; err != nil {
 		log.Println("conveyer finished with error:", err)
-
-	} else {
-		log.Println("conveyer finished")
+		return
 	}
+
+	log.Println("conveyer finished")
 }
--- timur.usachev/task-5/pkg/handlers/handlers.go
+++ timur.usachev/task-5/pkg/handlers/handlers.go
@@ -16,20 +16,25 @@ const (
 
 func PrefixDecoratorFunc(ctx context.Context, input chan string, output chan string) error {
 	defer close(output)
+
 	for {
 		select {
 		case <-ctx.Done():
 			return nil
+
 		case line, ok := <-input:
 			if !ok {
 				return nil
 			}
+
 			if strings.Contains(line, noDecoratorData) {
 				return ErrNoDecorator
 			}
+
 			if !strings.HasPrefix(line, textForDecoratorString) {
 				line = textForDecoratorString + line
 			}
+
 			select {
 			case <-ctx.Done():
 				return nil
@@ -41,23 +46,29 @@ func PrefixDecoratorFunc(ctx context.Context, input chan string, output chan str
 
 func MultiplexerFunc(ctx context.Context, inputs []chan string, output chan string) error {
 	defer close(output)
+
 	if len(inputs) == 0 {
 		return nil
 	}
-	for _, ch := range inputs {
-		ch := ch
-		go func() {
+
+	for _, inputCh := range inputs {
+		ch := inputCh
+
+		go func(chLocal chan string) {
 			for {
 				select {
 				case <-ctx.Done():
 					return
-				case line, ok := <-ch:
+
+				case line, ok := <-chLocal:
 					if !ok {
 						return
 					}
+
 					if strings.Contains(line, noMultiplexerData) {
 						continue
 					}
+
 					select {
 					case <-ctx.Done():
 						return
@@ -65,8 +76,9 @@ func MultiplexerFunc(ctx context.Context, inputs []chan string, output chan stri
 					}
 				}
 			}
-		}()
+		}(ch)
 	}
+
 	<-ctx.Done()
 	return nil
 }
@@ -77,23 +89,29 @@ func SeparatorFunc(ctx context.Context, input chan string, outputs []chan string
 			close(ch)
 		}
 	}()
-	cnt := 0
-	cntOut := len(outputs)
+
+	countOut := len(outputs)
+	index := 0
+
 	for {
 		select {
 		case <-ctx.Done():
 			return nil
+
 		case line, ok := <-input:
 			if !ok {
 				return nil
 			}
-			if cntOut != 0 {
-				select {
-				case <-ctx.Done():
-					return nil
-				case outputs[cnt%cntOut] <- line:
-					cnt++
-				}
+
+			if countOut == 0 {
+				continue
+			}
+
+			select {
+			case <-ctx.Done():
+				return nil
+			case outputs[index%countOut] <- line:
+				index++
 			}
 		}
 	}
