5f5a8816e50028706b727c76ca3fa147ef4eb759
MeguruMacabre <megurumacabre@gmail.com>
2025-11-27 23:57:39 +0300

    [TASK-5] fix lint 2

--- maksim.komarov/task-5/pkg/handlers/handlers.go
+++ maksim.komarov/task-5/pkg/handlers/handlers.go
@@ -2,26 +2,27 @@ package handlers
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"strings"
 	"sync"
 )
+
+var ErrCantBeDecorated = errors.New("can't be decorated")
 
 func PrefixDecoratorFunc(ctx context.Context, input chan string, output chan string) error {
 	for {
 		select {
 		case <-ctx.Done():
 			return fmt.Errorf("context canceled: %w", ctx.Err())
-		case v, ok := <-input:
+		case value, ok := <-input:
 			if !ok {
 				return nil
 			}
-
-			if strings.Contains(v, "no decorator") {
-				return fmt.Errorf("can't be decorated")
+			if strings.Contains(value, "no decorator") {
+				return ErrCantBeDecorated
 			}
-
-			output <- "decorated: " + v
+			output <- "decorated: " + value
 		}
 	}
 }
@@ -30,19 +31,16 @@ func SeparatorFunc(ctx context.Context, input chan string, outputs []chan string
 	if len(outputs) == 0 {
 		return nil
 	}
-
 	index := 0
-
 	for {
 		select {
 		case <-ctx.Done():
 			return fmt.Errorf("context canceled: %w", ctx.Err())
-		case v, ok := <-input:
+		case value, ok := <-input:
 			if !ok {
 				return nil
 			}
-
-			outputs[index] <- v
+			outputs[index] <- value
 			index++
 			if index == len(outputs) {
 				index = 0
@@ -54,30 +52,26 @@ func SeparatorFunc(ctx context.Context, input chan string, outputs []chan string
 func MultiplexerFunc(ctx context.Context, inputs []chan string, output chan string) error {
 	var waitGroup sync.WaitGroup
 	waitGroup.Add(len(inputs))
-
 	for _, ch := range inputs {
 		inCh := ch
-
 		go func() {
 			defer waitGroup.Done()
-
 			for {
 				select {
 				case <-ctx.Done():
 					return
-				case v, ok := <-inCh:
+				case value, ok := <-inCh:
 					if !ok {
 						return
 					}
-					if strings.Contains(v, "no multiplexer") {
+					if strings.Contains(value, "no multiplexer") {
 						continue
 					}
-					output <- v
+					output <- value
 				}
 			}
 		}()
 	}
-
 	waitGroup.Wait()
 	return nil
 }
