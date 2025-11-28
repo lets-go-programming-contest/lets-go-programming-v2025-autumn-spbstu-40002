46323d1ca11857dc607b75fdca11bf79cbb49c4e
Ivashin <danya001406@gmail.com>
2025-11-28 21:00:52 +0300

    [TASK-3] fix

--- danya.ivashin/task-3/internal/bank/bank.go
+++ danya.ivashin/task-3/internal/bank/bank.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"strconv"
-	"strings"
 
 	"github.com/Danya-byte/task-3/pkg/must"
 	"golang.org/x/text/encoding/charmap"
@@ -19,46 +17,13 @@ type Bank struct {
 }
 
 type Currency struct {
-	ID       string  `xml:"ID,attr" json:"id"`
-	NumCode  int     `xml:"NumCode" json:"num_code"`
-	CharCode string  `xml:"CharCode" json:"char_code"`
-	Value    float64 `json:"value"`
-	Nominal  int     `xml:"Nominal" json:"nominal"`
-	Name     string  `xml:"Name" json:"name"`
-	Rate     float64 `json:"rate"`
-}
-
-func (c *Currency) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
-	type raw Currency // избегаем рекурсии
-	var tmp struct {
-		raw
-		Value string `xml:"Value"`
-		Rate  string `xml:"VunitRate"`
-	}
-
-	if err := d.DecodeElement(&tmp, &start); err != nil {
-		return err
-	}
-
-	*c = Currency(tmp.raw)
-
-	if v := strings.Replace(tmp.Value, ",", ".", 1); v != "" {
-		val, err := strconv.ParseFloat(v, 64)
-		if err != nil {
-			return fmt.Errorf("invalid Value: %w", err)
-		}
-		c.Value = val
-	}
-
-	if v := strings.Replace(tmp.Rate, ",", ".", 1); v != "" {
-		rval, err := strconv.ParseFloat(v, 64)
-		if err != nil {
-			return fmt.Errorf("invalid Rate: %w", err)
-		}
-		c.Rate = rval
-	}
-
-	return nil
+	ID       string `xml:"ID,attr" json:"id"`
+	NumCode  int    `xml:"NumCode" json:"num_code"`
+	CharCode string `xml:"CharCode" json:"char_code"`
+	Value    string `xml:"Value" json:"value"`
+	Nominal  int    `xml:"Nominal" json:"nominal"`
+	Name     string `xml:"Name" json:"name"`
+	Rate     string `xml:"VunitRate" json:"rate"`
 }
 
 func charsetReader(charset string, input io.Reader) (io.Reader, error) {
@@ -72,12 +37,14 @@ func charsetReader(charset string, input io.Reader) (io.Reader, error) {
 
 func ParseXML(r io.Reader) (*Bank, error) {
 	decoder := xml.NewDecoder(r)
+
 	decoder.CharsetReader = charsetReader
 
 	bank := new(Bank)
-	if err := decoder.Decode(bank); err != nil {
+	if err := decoder.Decode(&bank); err != nil {
 		return nil, fmt.Errorf("decoding currency bank: %w", err)
 	}
+
 	return bank, nil
 }
 
@@ -86,6 +53,7 @@ func ParseFileXML(path string) (*Bank, error) {
 	if err != nil {
 		return nil, fmt.Errorf("open input file: %w", err)
 	}
+
 	defer must.Close(path, file)
 
 	return ParseXML(file)
