6cf79fee6e86e71c7a6b781a64a3f4aaa7e617d1
Ivashin <danya001406@gmail.com>
2025-11-28 21:00:52 +0300

    [TASK-3] fix

--- danya.ivashin/task-3/internal/bank/output.go
+++ danya.ivashin/task-3/internal/bank/output.go
@@ -7,41 +7,72 @@ import (
 	"os"
 	"path/filepath"
 	"sort"
+	"strconv"
+	"strings"
 
 	"github.com/Danya-byte/task-3/pkg/must"
 )
 
-func (b *Bank) SortByValueDesc() {
-	sort.Slice(b.Currencies, func(i, j int) bool {
-		return b.Currencies[i].Value > b.Currencies[j].Value
+const FilePermissions = 0o755
+
+type outputBank []Currency
+
+func fetchOutput(b *Bank) (outputBank, error) {
+	out := make(outputBank, len(b.Currencies))
+
+	for i, c := range b.Currencies {
+		v, err := strconv.ParseFloat(strings.Replace(c.Value, ",", ".", 1), 64)
+		if err != nil {
+			return nil, fmt.Errorf("invalid type of value: %w", err)
+		}
+
+		out[i] = c
+		out[i].Value = fmt.Sprintf("%.4f", v)
+	}
+
+	return out, nil
+}
+
+func (b outputBank) sortByValueDown() {
+	sort.Slice(b, func(i, j int) bool {
+		vi, _ := strconv.ParseFloat(b[i].Value, 64)
+		vj, _ := strconv.ParseFloat(b[j].Value, 64)
+		return vi > vj
 	})
 }
 
-func (b *Bank) EncodeJSON(w io.Writer) error {
-	b.SortByValueDesc()
+func (b *Bank) EncodeJSON(writer io.Writer) error {
+	out, err := fetchOutput(b)
+	if err != nil {
+		return err
+	}
 
-	enc := json.NewEncoder(w)
-	enc.SetIndent("", "  ")
+	out.sortByValueDown()
 
-	if err := enc.Encode(b); err != nil {
+	encoder := json.NewEncoder(writer)
+
+	encoder.SetIndent("", "  ")
+
+	if err := encoder.Encode(&out); err != nil {
 		return fmt.Errorf("encoding bank: %w", err)
 	}
+
 	return nil
 }
 
 func (b *Bank) EncodeFileJSON(path string) error {
-	const perm = 0o755
-
 	dir := filepath.Dir(path)
-	if err := os.MkdirAll(dir, perm); err != nil {
+
+	if err := os.MkdirAll(dir, FilePermissions); err != nil {
 		return fmt.Errorf("create dir: %w", err)
 	}
 
-	f, err := os.Create(path)
+	file, err := os.Create(path)
 	if err != nil {
 		return fmt.Errorf("create file: %w", err)
 	}
-	defer must.Close(path, f)
 
-	return b.EncodeJSON(f)
+	defer must.Close(path, file)
+
+	return b.EncodeJSON(file)
 }
