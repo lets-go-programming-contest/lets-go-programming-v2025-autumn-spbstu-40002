487cc5774a3bfe9aba14a12178832c4e95e09e8e
Expeline <kalashyan.artur@list.ru>
2025-12-01 02:00:43 +0300

    [TASK-5] Fix linter and cyclop

--- arthur.kalashyan/task-5/pkg/handlers/handlers.go
+++ arthur.kalashyan/task-5/pkg/handlers/handlers.go
@@ -17,24 +17,24 @@ func PrefixDecoratorFunc(
 	for {
 		select {
 		case <-ctx.Done():
-			return ctx.Err()
-		case value, ok := <-inputChan:
+			return errors.Join(ctx.Err())
+		case val, ok := <-inputChan:
 			if !ok {
 				return nil
 			}
 
-			if strings.Contains(value, "no decorator") {
+			if strings.Contains(val, "no decorator") {
 				return ErrCannotDecorate
 			}
 
-			if !strings.HasPrefix(value, "decorated: ") {
-				value = "decorated: " + value
+			if !strings.HasPrefix(val, "decorated: ") {
+				val = "decorated: " + val
 			}
 
 			select {
 			case <-ctx.Done():
-				return ctx.Err()
-			case outputChan <- value:
+				return errors.Join(ctx.Err())
+			case outputChan <- val:
 			}
 		}
 	}
@@ -49,7 +49,7 @@ func SeparatorFunc(
 		for {
 			select {
 			case <-ctx.Done():
-				return ctx.Err()
+				return errors.Join(ctx.Err())
 			case _, ok := <-inputChan:
 				if !ok {
 					return nil
@@ -62,19 +62,19 @@ func SeparatorFunc(
 	for {
 		select {
 		case <-ctx.Done():
-			return ctx.Err()
-		case value, ok := <-inputChan:
+			return errors.Join(ctx.Err())
+		case val, ok := <-inputChan:
 			if !ok {
 				return nil
 			}
 
-			outputChan := outputChans[idx%len(outputChans)]
+			out := outputChans[idx%len(outputChans)]
 			idx++
 
 			select {
 			case <-ctx.Done():
-				return ctx.Err()
-			case outputChan <- value:
+				return errors.Join(ctx.Err())
+			case out <- val:
 			}
 		}
 	}
@@ -85,36 +85,36 @@ func MultiplexerFunc(
 	inputChans []chan string,
 	outputChan chan string,
 ) error {
-	var waitGroup sync.WaitGroup
+	var wg sync.WaitGroup
 
-	for _, channel := range inputChans {
-		waitGroup.Add(1)
-		ch := channel
+	for _, ch := range inputChans {
+		chCopy := ch
+		wg.Add(1)
 		go func() {
-			defer waitGroup.Done()
+			defer wg.Done()
 			for {
 				select {
 				case <-ctx.Done():
 					return
-				case value, ok := <-ch:
+				case val, ok := <-chCopy:
 					if !ok {
 						return
 					}
 
-					if strings.Contains(value, "no multiplexer") {
+					if strings.Contains(val, "no multiplexer") {
 						continue
 					}
 
 					select {
 					case <-ctx.Done():
 						return
-					case outputChan <- value:
+					case outputChan <- val:
 					}
 				}
 			}
 		}()
 	}
 
-	waitGroup.Wait()
+	wg.Wait()
 	return nil
 }
