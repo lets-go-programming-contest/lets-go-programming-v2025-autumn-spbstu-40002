b401740ff78bccd3a251c11b6996c0c99af0aac1
Платон Карякин <viceone.von@gmail.com>
2025-12-02 19:53:06 +0300

    [TASK-3] Update output.go 4

--- platon.karyakin/task-3/internal/bank/output.go
+++ platon.karyakin/task-3/internal/bank/output.go
@@ -12,7 +12,7 @@ import (
 )
 
 type outputCurrency struct {
-	NumCode  int     `json:"num_code"
+	NumCode  int     `json:"num_code"`
 	CharCode string  `json:"char_code"`
 	Value    float64 `json:"value"`
 }
