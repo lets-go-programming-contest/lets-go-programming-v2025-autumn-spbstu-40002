59ef741b410fa0031f095604065a6a92ca1990a3
Karmanova Alyona <karaliona2006@gmail.com>
2025-12-03 09:47:41 +0300

    [TASK-5] Initial commit”

---
+++ alyona.karmanova/task-5/pkg/conveyer/conveyerclass.go
@@ -0,0 +1,149 @@
+package conveyer
+
+import (
+	"context"
+	"errors"
+	"sync"
+)
+
+var ErrChanNotFound = errors.New("chan not found")
+
+const Undefined = "undefined"
+
+type Conveyer struct {
+	size     int
+	channels map[string]chan string
+	handlers []func(ctx context.Context) error
+	mutex    sync.RWMutex
+}
+
+func New(size int) *Conveyer {
+	return &Conveyer{
+		size:     size,
+		channels: make(map[string]chan string),
+		handlers: []func(ctx context.Context) error{},
+		mutex:    sync.RWMutex{},
+	}
+}
+
+func (c *Conveyer) reserveChannel(name string) chan string {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	ch, ok := c.channels[name]
+	if !ok {
+		ch = make(chan string, c.size)
+		c.channels[name] = ch
+	}
+	return ch
+}
+
+func (c *Conveyer) RegisterDecorator(fn func(ctx context.Context, input, output chan string) error, input, output string) {
+	inCh := c.reserveChannel(input)
+	outCh := c.reserveChannel(output)
+
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	c.handlers = append(c.handlers, func(ctx context.Context) error {
+		return fn(ctx, inCh, outCh)
+	})
+}
+
+func (c *Conveyer) RegisterMultiplexer(fn func(ctx context.Context, inputs []chan string, output chan string) error, inputs []string, output string) {
+	inChs := make([]chan string, len(inputs))
+	for i, name := range inputs {
+		inChs[i] = c.reserveChannel(name)
+	}
+	outCh := c.reserveChannel(output)
+
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	c.handlers = append(c.handlers, func(ctx context.Context) error {
+		return fn(ctx, inChs, outCh)
+	})
+}
+
+func (c *Conveyer) RegisterSeparator(fn func(ctx context.Context, input chan string, outputs []chan string) error, input string, outputs []string) {
+	inCh := c.reserveChannel(input)
+	outChs := make([]chan string, len(outputs))
+	for i, name := range outputs {
+		outChs[i] = c.reserveChannel(name)
+	}
+
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	c.handlers = append(c.handlers, func(ctx context.Context) error {
+		return fn(ctx, inCh, outChs)
+	})
+}
+
+func (c *Conveyer) Run(ctx context.Context) error {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	var wg sync.WaitGroup
+	errCh := make(chan error, 1)
+
+	launch := func(handler func(ctx context.Context) error) {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if err := handler(ctx); err != nil && !errors.Is(err, context.Canceled) && !errors.Is(err, context.DeadlineExceeded) {
+				select {
+				case errCh <- err:
+				default:
+				}
+			}
+		}()
+	}
+
+	c.mutex.RLock()
+	for _, h := range c.handlers {
+		launch(h)
+	}
+	c.mutex.RUnlock()
+
+	wg.Wait()
+
+	c.mutex.Lock()
+	for _, ch := range c.channels {
+		close(ch)
+	}
+	c.mutex.Unlock()
+
+	select {
+	case err := <-errCh:
+		return err
+	default:
+		return nil
+	}
+}
+
+func (c *Conveyer) Send(input string, data string) error {
+	c.mutex.RLock()
+	ch, ok := c.channels[input]
+	c.mutex.RUnlock()
+
+	if !ok {
+		return ErrChanNotFound
+	}
+
+	ch <- data
+	return nil
+}
+
+func (c *Conveyer) Recv(output string) (string, error) {
+	c.mutex.RLock()
+	ch, ok := c.channels[output]
+	c.mutex.RUnlock()
+
+	if !ok {
+		return "", ErrChanNotFound
+	}
+
+	data, ok := <-ch
+	if !ok {
+		return Undefined, nil
+	}
+	return data, nil
+}
---
+++ alyona.karmanova/task-5/pkg/handlers/sepatatorfunc.go
@@ -0,0 +1,36 @@
+package handlers
+
+import "context"
+
+type Separator func(
+	context.Context,
+	chan string,
+	[]chan string,
+) error
+
+func SeparatorFunc(ctx context.Context, input chan string, outputs []chan string) error {
+	cnt := 0
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+
+		case val, ok := <-input:
+			if !ok {
+				return nil
+			}
+
+			out := outputs[cnt%len(outputs)]
+
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+
+			case out <- val:
+			}
+
+			cnt++
+		}
+	}
+
+}
