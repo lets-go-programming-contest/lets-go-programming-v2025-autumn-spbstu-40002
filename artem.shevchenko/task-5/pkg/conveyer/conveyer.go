6509f89d02e1d816acacf65322fa7c991ece2669
Artyom Shevchenko <shevchenko2.ao@edu.spbstu.ru>
2025-12-05 00:57:18 +0300

    [TASK-5] war with lint

--- artem.shevchenko/task-5/pkg/conveyer/conveyer.go
+++ artem.shevchenko/task-5/pkg/conveyer/conveyer.go
@@ -1,109 +1,118 @@
 package conveyer
 
 import (
-    "context"
-    
-    "golang.org/x/sync/errgroup"
+	"context"
+	"fmt"
+
+	"golang.org/x/sync/errgroup"
 )
 
 type Conveyer struct {
-    channelSize  int
-    channels     map[string]chan string
-    handlersPool []func(context.Context) error
+	channelSize  int
+	channels     map[string]chan string
+	handlersPool []func(context.Context) error
 }
 
 func New(channelSize int) *Conveyer {
-    return &Conveyer{
-        channelSize:  channelSize,
-        channels:     make(map[string]chan string),
-        handlersPool: make([]func(context.Context) error, 0),
-    }
+	return &Conveyer{
+		channelSize:  channelSize,
+		channels:     make(map[string]chan string),
+		handlersPool: make([]func(context.Context) error, 0),
+	}
 }
 
 func (conveyer *Conveyer) makeChannels(names ...string) {
-    for _, name := range names {
-        if _, channelExists := conveyer.channels[name]; !channelExists {
-            conveyer.channels[name] = make(chan string, conveyer.channelSize)
-        }
-    }
+	for _, name := range names {
+		if _, channelExists := conveyer.channels[name]; !channelExists {
+			conveyer.channels[name] = make(chan string, conveyer.channelSize)
+		}
+	}
 }
 
 func (conveyer *Conveyer) RegisterDecorator(
-    fn func(ctx context.Context, input chan string, output chan string) error,
-    input string,
-    output string,
+	fn func(ctx context.Context, input chan string, output chan string) error,
+	input string,
+	output string,
 ) {
-    conveyer.makeChannels(input, output)
-    conveyer.handlersPool = append(conveyer.handlersPool, func(ctx context.Context) error {
-        return fn(ctx, conveyer.channels[input], conveyer.channels[output])
-    })
+	conveyer.makeChannels(input, output)
+	conveyer.handlersPool = append(conveyer.handlersPool, func(ctx context.Context) error {
+		return fn(ctx, conveyer.channels[input], conveyer.channels[output])
+	})
 }
 
 func (conveyer *Conveyer) RegisterMultiplexer(
-    fn func(ctx context.Context, inputs []chan string, output chan string) error,
-    inputs []string,
-    output string,
+	fn func(ctx context.Context, inputs []chan string, output chan string) error,
+	inputs []string,
+	output string,
 ) {
-    conveyer.makeChannels(inputs...)
-    conveyer.makeChannels(output)
-    conveyer.handlersPool = append(conveyer.handlersPool, func(ctx context.Context) error {
-        inputChannels := make([]chan string, len(inputs))
-        for index, input := range inputs {
-            inputChannels[index] = conveyer.channels[input]
-        }
-        return fn(ctx, inputChannels, conveyer.channels[output])
-    })
+	conveyer.makeChannels(inputs...)
+	conveyer.makeChannels(output)
+	conveyer.handlersPool = append(conveyer.handlersPool, func(ctx context.Context) error {
+		inputChannels := make([]chan string, len(inputs))
+		for index, input := range inputs {
+			inputChannels[index] = conveyer.channels[input]
+		}
+
+		return fn(ctx, inputChannels, conveyer.channels[output])
+	})
 }
 
 func (conveyer *Conveyer) RegisterSeparator(
-    fn func(ctx context.Context, input chan string, outputs []chan string) error,
-    input string,
-    outputs []string,
+	fn func(ctx context.Context, input chan string, outputs []chan string) error,
+	input string,
+	outputs []string,
 ) {
-    conveyer.makeChannels(input)
-    conveyer.makeChannels(outputs...)
-    conveyer.handlersPool = append(conveyer.handlersPool, func(ctx context.Context) error {
-        outputChannels := make([]chan string, len(outputs))
-        for index, output := range outputs {
-            outputChannels[index] = conveyer.channels[output]
-        }
-        return fn(ctx, conveyer.channels[input], outputChannels)
-    })
+	conveyer.makeChannels(input)
+	conveyer.makeChannels(outputs...)
+	conveyer.handlersPool = append(conveyer.handlersPool, func(ctx context.Context) error {
+		outputChannels := make([]chan string, len(outputs))
+		for index, output := range outputs {
+			outputChannels[index] = conveyer.channels[output]
+		}
+
+		return fn(ctx, conveyer.channels[input], outputChannels)
+	})
 }
 
 func (conveyer *Conveyer) Run(ctx context.Context) error {
-    handlersGroup, handlersContext := errgroup.WithContext(ctx)
-    
-    for _, handler := range conveyer.handlersPool {
-        currentHandler := handler
-        handlersGroup.Go(func() error {
-            return currentHandler(handlersContext)
-        })
-    }
-    
-    return handlersGroup.Wait()
+	handlersGroup, handlersContext := errgroup.WithContext(ctx)
+
+	for _, handler := range conveyer.handlersPool {
+		currentHandler := handler
+
+		handlersGroup.Go(func() error {
+			return currentHandler(handlersContext)
+		})
+	}
+
+	if err := handlersGroup.Wait(); err != nil {
+		return fmt.Errorf("handlers group wait error: %w", err)
+	}
+
+	return nil
 }
 
 func (conveyer *Conveyer) Send(input string, data string) error {
-    channel, channelExists := conveyer.channels[input]
-    if !channelExists {
-        return ErrChanNotFound
-    }
-    
-    channel <- data
-    return nil
+	channel, channelExists := conveyer.channels[input]
+	if !channelExists {
+		return ErrChanNotFound
+	}
+
+	channel <- data
+
+	return nil
 }
 
 func (conveyer *Conveyer) Recv(output string) (string, error) {
-    channel, channelExists := conveyer.channels[output]
-    if !channelExists {
-        return "", ErrChanNotFound
-    }
-    
-    data, channelOpen := <-channel
-    if !channelOpen {
-        return UndefinedData, nil
-    }
-    
-    return data, nil
+	channel, channelExists := conveyer.channels[output]
+	if !channelExists {
+		return "", ErrChanNotFound
+	}
+
+	data, channelOpen := <-channel
+	if !channelOpen {
+		return UndefinedData, nil
+	}
+
+	return data, nil
 }
--- artem.shevchenko/task-5/pkg/handlers/multiplexer.go
+++ artem.shevchenko/task-5/pkg/handlers/multiplexer.go
@@ -1,36 +1,36 @@
 package handlers
 
 import (
-    "context"
-    "strings"
+	"context"
+	"strings"
 )
 
 func MultiplexerFunc(ctx context.Context, inputs []chan string, output chan string) error {
-    defer close(output)
-    
-    for _, inputChannel := range inputs {
-        go func(channel chan string) {
-            for {
-                select {
-                case <-ctx.Done():
-                    return
-                case data, channelOpen := <-channel:
-                    if !channelOpen {
-                        return
-                    }
-                    
-                    if !strings.Contains(data, NoMultiplexer) {
-                        select {
-                        case <-ctx.Done():
-                            return
-                        case output <- data:
-                        }
-                    }
-                }
-            }
-        }(inputChannel)
-    }
-    
-    <-ctx.Done()
-    return nil
+	defer close(output)
+
+	for _, inputChannel := range inputs {
+		go func(channel chan string) {
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				case data, channelOpen := <-channel:
+					if !channelOpen {
+						return
+					}
+
+					if !strings.Contains(data, NoMultiplexer) {
+						select {
+						case <-ctx.Done():
+							return
+						case output <- data:
+						}
+					}
+				}
+			}
+		}(inputChannel)
+	}
+
+	<-ctx.Done()
+	return nil
 }
