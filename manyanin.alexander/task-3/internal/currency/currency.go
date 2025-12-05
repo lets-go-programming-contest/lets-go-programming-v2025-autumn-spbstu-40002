e35383923118d8f3b350431885ca5e5f686b87de
Alexander Manyanin <almen69@yandex.ru>
2025-12-05 17:55:58 +0300

    [TASK-3] remove panic from config, add defolt value to config, unite xml and json struct and fix

--- manyanin.alexander/task-3/internal/currency/currency.go
+++ manyanin.alexander/task-3/internal/currency/currency.go
@@ -1,40 +1,51 @@
 package currency
 
 import (
+	"encoding/json"
 	"sort"
 	"strconv"
 	"strings"
 )
 
 type Currency struct {
-	NumCode  string  `xml:"NumCode" json:"num_code"`
-	CharCode string  `xml:"CharCode" json:"char_code"`
-	Value    string  `xml:"Value" json:"-"`
-	ValueNum float64 `xml:"-" json:"value"`
+	NumCode  string `xml:"NumCode"`
+	CharCode string `xml:"CharCode"`
+	Value    string `xml:"Value"`
 }
 
-func ValuteToCurr(valute Currency) *Currency {
-	cleanValue := strings.ReplaceAll(valute.Value, ",", ".")
+func (c Currency) MarshalJSON() ([]byte, error) {
+	cleanValue := strings.ReplaceAll(c.Value, ",", ".")
 
-	value, err := strconv.ParseFloat(cleanValue, 64)
-	if err != nil {
-		value = 0
-	}
+	value, _ := strconv.ParseFloat(cleanValue, 64)
 
-	return &Currency{
-		NumCode:  valute.NumCode,
-		CharCode: valute.CharCode,
-		Value:    valute.Value,
-		ValueNum: value,
-	}
+	numCode, _ := strconv.Atoi(c.NumCode)
+
+	return json.Marshal(struct {
+		NumCode  int     `json:"num_code"`
+		CharCode string  `json:"char_code"`
+		Value    float64 `json:"value"`
+	}{
+		NumCode:  numCode,
+		CharCode: c.CharCode,
+		Value:    value,
+	})
+}
+
+func parseValue(value string) float64 {
+	cleanValue := strings.ReplaceAll(value, ",", ".")
+
+	val, _ := strconv.ParseFloat(cleanValue, 64)
+
+	return val
 }
 
 func SortByValue(currencies []Currency) []Currency {
 	sorted := make([]Currency, len(currencies))
+
 	copy(sorted, currencies)
 
 	sort.Slice(sorted, func(i, j int) bool {
-		return sorted[i].ValueNum > sorted[j].ValueNum
+		return parseValue(sorted[i].Value) > parseValue(sorted[j].Value)
 	})
 
 	return sorted
