76e83f01285cd1a1be3d08282c76c51a80bf50c9
Leon-Maryankov <mariannnkovleonid@gmail.com>
2025-12-05 18:07:34 +0300

    [TASK-5] fix lint

--- leonid.maryankov/task-5/pkg/conveyer/conveyer.go
+++ leonid.maryankov/task-5/pkg/conveyer/conveyer.go
@@ -2,162 +2,162 @@ package conveyer
 
 import (
 	"context"
-	"fmt"
+	"errors"
 	"sync"
 
 	"golang.org/x/sync/errgroup"
 )
 
-type handlerRunner func(ctx context.Context) error
+type HandlerRunner func(ctx context.Context) error
 
-type channel struct {
-	ch chan string
+type Channel struct {
+	Ch chan string
 }
 
 type ConveyorImpl struct {
-	size int
-
-	mu      sync.RWMutex
-	chans   map[string]*channel
-	runners []handlerRunner
+	Size    int
+	Mu      sync.RWMutex
+	Chans   map[string]*Channel
+	Runners []HandlerRunner
 }
+
+var ErrChanNotFound = errors.New("chan not found")
 
 func New(size int) *ConveyorImpl {
 	return &ConveyorImpl{
-		size:  size,
-		chans: make(map[string]*channel),
+		Size:    size,
+		Mu:      sync.RWMutex{},
+		Chans:   make(map[string]*Channel),
+		Runners: make([]HandlerRunner, 0),
 	}
 }
 
-func (c *ConveyorImpl) getOrCreate(id string) chan string {
-	if id == "" {
+func (c *ConveyorImpl) getOrCreate(channelID string) chan string {
+	if channelID == "" {
 		return nil
 	}
 
-	c.mu.Lock()
-	defer c.mu.Unlock()
+	c.Mu.Lock()
+	defer c.Mu.Unlock()
 
-	existing, ok := c.chans[id]
-	if !ok {
-		newCh := make(chan string, c.size)
-		c.chans[id] = &channel{ch: newCh}
-		return newCh
+	existing, exists := c.Chans[channelID]
+	if !exists {
+		newChan := make(chan string, c.Size)
+		c.Chans[channelID] = &Channel{Ch: newChan}
+
+		return newChan
 	}
 
-	return existing.ch
-
+	return existing.Ch
 }
 
 func (c *ConveyorImpl) RegisterDecorator(
-	fn func(context.Context, chan string, chan string) error,
-	input string,
-	output string,
+	handlerFunc func(context.Context, chan string, chan string) error,
+	inputID string,
+	outputID string,
 ) {
-	inCh := c.getOrCreate(input)
-	outCh := c.getOrCreate(output)
+	inputCh := c.getOrCreate(inputID)
+	outputCh := c.getOrCreate(outputID)
 
-	c.runners = append(c.runners, func(ctx context.Context) error {
-		return fn(ctx, inCh, outCh)
+	c.Runners = append(c.Runners, func(ctx context.Context) error {
+		return handlerFunc(ctx, inputCh, outputCh)
 	})
-
 }
 
 func (c *ConveyorImpl) RegisterSeparator(
-	fn func(context.Context, chan string, []chan string) error,
-	input string,
-	outputs []string,
+	handlerFunc func(context.Context, chan string, []chan string) error,
+	inputID string,
+	outputIDs []string,
 ) {
-	inCh := c.getOrCreate(input)
+	inputCh := c.getOrCreate(inputID)
 
-	outChs := make([]chan string, 0, len(outputs))
-	for _, id := range outputs {
-		outChs = append(outChs, c.getOrCreate(id))
+	outputChs := make([]chan string, 0, len(outputIDs))
+	for _, id := range outputIDs {
+		outputChs = append(outputChs, c.getOrCreate(id))
 	}
 
-	c.runners = append(c.runners, func(ctx context.Context) error {
-		return fn(ctx, inCh, outChs)
+	c.Runners = append(c.Runners, func(ctx context.Context) error {
+		return handlerFunc(ctx, inputCh, outputChs)
 	})
-
 }
 
 func (c *ConveyorImpl) RegisterMultiplexer(
-	fn func(context.Context, []chan string, chan string) error,
-	inputs []string,
-	output string,
+	handlerFunc func(context.Context, []chan string, chan string) error,
+	inputIDs []string,
+	outputID string,
 ) {
-	inChs := make([]chan string, 0, len(inputs))
-	for _, id := range inputs {
-		inChs = append(inChs, c.getOrCreate(id))
+	inputChs := make([]chan string, 0, len(inputIDs))
+	for _, id := range inputIDs {
+		inputChs = append(inputChs, c.getOrCreate(id))
 	}
 
-	outCh := c.getOrCreate(output)
+	outputCh := c.getOrCreate(outputID)
 
-	c.runners = append(c.runners, func(ctx context.Context) error {
-		return fn(ctx, inChs, outCh)
+	c.Runners = append(c.Runners, func(ctx context.Context) error {
+		return handlerFunc(ctx, inputChs, outputCh)
 	})
-
 }
 
 func (c *ConveyorImpl) Run(ctx context.Context) error {
-	g, ctx := errgroup.WithContext(ctx)
+	group, ctx := errgroup.WithContext(ctx)
 
-	for i := range c.runners {
-		r := c.runners[i]
-		g.Go(func() error {
-			return r(ctx)
+	for i := range c.Runners {
+		runner := c.Runners[i]
+
+		runnerLocal := runner
+		group.Go(func() error {
+			return runnerLocal(ctx)
 		})
 	}
 
-	err := g.Wait()
+	err := group.Wait()
+
 	c.closeAll()
 
 	return err
-
 }
 
 func (c *ConveyorImpl) closeAll() {
-	c.mu.Lock()
-	defer c.mu.Unlock()
+	c.Mu.Lock()
+	defer c.Mu.Unlock()
 
-	for _, chObj := range c.chans {
+	for _, chObj := range c.Chans {
 		func(ch chan string) {
-			defer func() { _ = recover() }()
+			defer func() {
+				_ = recover()
+			}()
 			close(ch)
-		}(chObj.ch)
+		}(chObj.Ch)
 	}
-
 }
 
-var ErrChanNotFound = fmt.Errorf("chan not found")
+func (c *ConveyorImpl) Send(inputID string, data string) error {
+	c.Mu.RLock()
+	channelObj, exists := c.Chans[inputID]
+	c.Mu.RUnlock()
 
-func (c *ConveyorImpl) Send(input string, data string) error {
-	c.mu.RLock()
-	chObj, ok := c.chans[input]
-	c.mu.RUnlock()
-
-	if !ok {
+	if !exists {
 		return ErrChanNotFound
 	}
 
-	chObj.ch <- data
-	return nil
+	channelObj.Ch <- data
 
+	return nil
 }
 
-func (c *ConveyorImpl) Recv(output string) (string, error) {
-	c.mu.RLock()
-	chObj, ok := c.chans[output]
-	c.mu.RUnlock()
+func (c *ConveyorImpl) Recv(outputID string) (string, error) {
+	c.Mu.RLock()
+	channelObj, exists := c.Chans[outputID]
+	c.Mu.RUnlock()
 
-	if !ok {
+	if !exists {
 		return "", ErrChanNotFound
 	}
 
-	value, ok := <-chObj.ch
-	if !ok {
+	value, open := <-channelObj.Ch
+	if !open {
 		return "undefined", nil
 	}
 
 	return value, nil
-
 }
