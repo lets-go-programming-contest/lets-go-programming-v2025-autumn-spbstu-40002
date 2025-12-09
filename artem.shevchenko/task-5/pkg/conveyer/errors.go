948e773f68ffb57c35918214a01449c773d80a02
Artyom Shevchenko <shevchenko2.ao@edu.spbstu.ru>
2025-12-09 03:32:29 +0300

    [TASK-5] war with lint x8

--- artem.shevchenko/task-5/pkg/conveyer/errors.go
+++ artem.shevchenko/task-5/pkg/conveyer/errors.go
@@ -2,7 +2,4 @@ package conveyer
 
 import "errors"
 
-var (
-	ErrChanNotFound      = errors.New("chan not found")
-	ErrHandlersGroupWait = errors.New("handlers group wait error")
-)
+var ErrChanNotFound = errors.New("chan not found")
