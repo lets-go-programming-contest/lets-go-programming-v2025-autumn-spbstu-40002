eba9f09b8769084cbe9772fc05a6e21ccbd52d1c
Artyom Shevchenko <shevchenko2.ao@edu.spbstu.ru>
2025-12-09 03:22:57 +0300

    [TASK-5] war with lint x6

--- artem.shevchenko/task-5/pkg/handlers/errors.go
+++ artem.shevchenko/task-5/pkg/handlers/errors.go
@@ -2,4 +2,7 @@ package handlers
 
 import "errors"
 
-var ErrCantBeDecorated = errors.New("can't be decorated")
+var (
+	ErrCantBeDecorated    = errors.New("can't be decorated")
+	ErrHandlersGroupWait  = errors.New("handlers group wait error")
+)
