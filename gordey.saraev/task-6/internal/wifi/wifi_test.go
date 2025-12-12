028fa1e028d086f86c5e40ed07b4532246054801
Сараев Гордей <isisu7337@gmail.com>
2025-12-12 20:13:36 +0300

    [TASK-6] FIX MB

--- gordey.saraev/task-6/internal/wifi/wifi_test.go
+++ gordey.saraev/task-6/internal/wifi/wifi_test.go
@@ -17,6 +17,11 @@ type MockWiFiHandle struct {
 
 func (m *MockWiFiHandle) Interfaces() ([]*mdlayherWifi.Interface, error) {
 	args := m.Called()
+
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+
 	return args.Get(0).([]*mdlayherWifi.Interface), args.Error(1)
 }
 
