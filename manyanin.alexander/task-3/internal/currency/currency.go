c49c30870456db5dbeac828d442a29dc910ea4b9
Alexander Manyanin <almen69@yandex.ru>
2025-12-13 14:52:47 +0300

    [TASK-3] Change struct in currency

--- manyanin.alexander/task-3/internal/currency/currency.go
+++ manyanin.alexander/task-3/internal/currency/currency.go
@@ -1,61 +1,85 @@
 package currency
 
 import (
+	"encoding/json"
 	"encoding/xml"
 	"fmt"
 	"sort"
 	"strconv"
 	"strings"
+
+	"github.com/manyanin.alexander/task-3/internal/errors"
 )
 
 type Currency struct {
-	XMLName  xml.Name `xml:"Valute"`
-	NumCode  int      `json:"num_code"  xml:"NumCode"`
+	XMLName  xml.Name `json:"-"         xml:"Valute"`
+	NumCode  string   `json:"num_code"  xml:"NumCode"`
 	CharCode string   `json:"char_code" xml:"CharCode"`
-	Value    float64  `json:"value"     xml:"Value"`
+	Value    string   `json:"value"     xml:"Value"`
 }
 
-func (c *Currency) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
-	type currencyXML struct {
-		NumCode  string `xml:"NumCode"`
-		CharCode string `xml:"CharCode"`
-		Value    string `xml:"Value"`
+func (c *Currency) UnmarshalXML(decoder *xml.Decoder, start xml.StartElement) error {
+	type alias Currency
+	var aux struct {
+		alias
+		NumCodeStr string `xml:"NumCode"`
+		ValueStr   string `xml:"Value"`
 	}
 
-	var aux currencyXML
-	if err := d.DecodeElement(&aux, &start); err != nil {
-		return err
+	if err := decoder.DecodeElement(&aux, &start); err != nil {
+		return fmt.Errorf("%w: %w", errors.ErrXMLDecode, err)
 	}
 
-	numCode, err := strconv.Atoi(aux.NumCode)
-	if err != nil {
-		return fmt.Errorf("failed to parse NumCode: %w", err)
-	}
+	*c = Currency(aux.alias)
 
-	c.NumCode = numCode
+	c.NumCode = aux.NumCodeStr
 
-	cleanValue := strings.ReplaceAll(aux.Value, ",", ".")
-
-	value, err := strconv.ParseFloat(cleanValue, 64)
-	if err != nil {
-		return fmt.Errorf("failed to parse Value: %w", err)
-	}
-
-	c.Value = value
+	c.Value = aux.ValueStr
 
 	c.CharCode = aux.CharCode
-
-	c.XMLName = start.Name
 
 	return nil
 }
 
+func (c Currency) MarshalJSON() ([]byte, error) {
+	cleanValue := strings.ReplaceAll(c.Value, ",", ".")
+
+	value, _ := strconv.ParseFloat(cleanValue, 64)
+
+	numCode, _ := strconv.Atoi(c.NumCode)
+
+	data, err := json.Marshal(struct {
+		NumCode  int     `json:"num_code"`
+		CharCode string  `json:"char_code"`
+		Value    float64 `json:"value"`
+	}{
+		NumCode:  numCode,
+		CharCode: c.CharCode,
+		Value:    value,
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("%w: %w", errors.ErrJSONMarshal, err)
+	}
+
+	return data, nil
+}
+
+func parseValue(value string) float64 {
+	cleanValue := strings.ReplaceAll(value, ",", ".")
+
+	val, _ := strconv.ParseFloat(cleanValue, 64)
+
+	return val
+}
+
 func SortByValue(currencies []Currency) []Currency {
 	sorted := make([]Currency, len(currencies))
+
 	copy(sorted, currencies)
 
 	sort.Slice(sorted, func(i, j int) bool {
-		return sorted[i].Value > sorted[j].Value
+		return parseValue(sorted[i].Value) > parseValue(sorted[j].Value)
 	})
 
 	return sorted
