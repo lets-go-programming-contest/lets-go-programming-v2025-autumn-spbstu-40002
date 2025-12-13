904c0481187839094a9b85d09a1b8778f8cd4e99
Alexander Manyanin <almen69@yandex.ru>
2025-12-13 14:39:47 +0300

    [TASK-3] Change struct in currency

--- manyanin.alexander/task-3/internal/currency/currency.go
+++ manyanin.alexander/task-3/internal/currency/currency.go
@@ -9,40 +9,43 @@ import (
 )
 
 type Currency struct {
-	XMLName  xml.Name `json:"-"         xml:"Valute"`
+	XMLName  xml.Name `xml:"Valute"`
 	NumCode  int      `json:"num_code"  xml:"NumCode"`
 	CharCode string   `json:"char_code" xml:"CharCode"`
 	Value    float64  `json:"value"     xml:"Value"`
 }
 
 func (c *Currency) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
-	type alias Currency
-	var aux struct {
-		alias
-		NumCodeStr string `xml:"NumCode"`
-		ValueStr   string `xml:"Value"`
+	type currencyXML struct {
+		NumCode  string `xml:"NumCode"`
+		CharCode string `xml:"CharCode"`
+		Value    string `xml:"Value"`
 	}
 
+	var aux currencyXML
 	if err := d.DecodeElement(&aux, &start); err != nil {
 		return err
 	}
 
-	*c = Currency(aux.alias)
-
-	numCode, err := strconv.Atoi(aux.NumCodeStr)
+	numCode, err := strconv.Atoi(aux.NumCode)
 	if err != nil {
 		return fmt.Errorf("failed to parse NumCode: %w", err)
 	}
+
 	c.NumCode = numCode
 
-	cleanValue := strings.ReplaceAll(aux.ValueStr, ",", ".")
+	cleanValue := strings.ReplaceAll(aux.Value, ",", ".")
+
 	value, err := strconv.ParseFloat(cleanValue, 64)
 	if err != nil {
 		return fmt.Errorf("failed to parse Value: %w", err)
 	}
+
 	c.Value = value
 
 	c.CharCode = aux.CharCode
+
+	c.XMLName = start.Name
 
 	return nil
 }
