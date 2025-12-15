b0e2a1c02bb70f360602e0361188c3bb0c0a3206
CaLLIaay <opukkuyt@gmail.com>
2025-12-15 23:28:24 +0300

    [TASK-2-2] fix

--- stepanov.alexander/task-2-2/cmd/service/main.go
+++ stepanov.alexander/task-2-2/cmd/service/main.go
@@ -7,53 +7,60 @@ import (
 
 type IntHeap []int
 
-func (h IntHeap) Len() int {
-	return len(h)
-}
-func (h IntHeap) Less(i, j int) bool {
-	return h[i] > h[j]
-}
-func (h IntHeap) Swap(i, j int) {
-	h[i], h[j] = h[j], h[i]
-}
+func (h *IntHeap) Len() int           { return len(*h) }
+func (h *IntHeap) Less(i, j int) bool { return (*h)[i] > (*h)[j] }
+func (h *IntHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
 
 func (h *IntHeap) Push(x any) {
-	v := x.(int)
-	*h = append(*h, v)
+	value, ok := x.(int)
+	if !ok {
+		return
+	}
+
+	*h = append(*h, value)
 }
 
 func (h *IntHeap) Pop() any {
 	old := *h
 	n := len(old)
-	x := old[n-1]
+	value := old[n-1]
 	*h = old[:n-1]
-	return x
+
+	return value
 }
 
 func main() {
-	var n, k int
-	if _, err := fmt.Scan(&n); err != nil {
+	var dishesCount, order int
+
+	if _, err := fmt.Scan(&dishesCount); err != nil {
 		return
 	}
 
 	h := &IntHeap{}
 	heap.Init(h)
 
-	for i := 0; i < n; i++ {
-		var x int
-		if _, err := fmt.Scan(&x); err != nil {
+	for i := 0; i < dishesCount; i++ {
+		var rating int
+
+		if _, err := fmt.Scan(&rating); err != nil {
 			return
 		}
-		heap.Push(h, x)
+
+		heap.Push(h, rating)
 	}
 
-	if _, err := fmt.Scan(&k); err != nil {
+	if _, err := fmt.Scan(&order); err != nil {
 		return
 	}
 
-	for i := 1; i < k; i++ {
+	for i := 1; i < order; i++ {
 		heap.Pop(h)
 	}
 
-	fmt.Println(heap.Pop(h).(int))
+	top, ok := heap.Pop(h).(int)
+	if !ok {
+		return
+	}
+
+	fmt.Println(top)
 }
