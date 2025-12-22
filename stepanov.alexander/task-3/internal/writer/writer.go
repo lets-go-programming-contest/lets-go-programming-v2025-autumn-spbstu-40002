59561ab44e0701db77d9ac29c89b6514695defb9
CaLLIaay <opukkuyt@gmail.com>
2025-12-22 18:09:11 +0300

    [TASK-3] fix writer dirs

--- stepanov.alexander/task-3/internal/writer/writer.go
+++ stepanov.alexander/task-3/internal/writer/writer.go
@@ -3,20 +3,24 @@ package writer
 import (
 	"encoding/json"
 	"os"
+	"path/filepath"
 
-	"stepanov.alexander/task-3/internal/processor"
+	"stepanov.Alexander/task-3/internal/processor"
 )
 
-func WriteJSON(filepath string, rates []processor.CurrencyRate) error {
+func WriteJSON(path string, rates []processor.CurrencyRate) error {
+	// создаём директорию, если её нет (.output, .output/subdir/one_more и т.п.)
+	dir := filepath.Dir(path)
+	if dir != "." && dir != "" {
+		if err := os.MkdirAll(dir, 0o755); err != nil {
+			return err
+		}
+	}
+
 	data, err := json.MarshalIndent(rates, "", "  ")
 	if err != nil {
 		return err
 	}
 
-	err = os.WriteFile(filepath, data, 0o644)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return os.WriteFile(path, data, 0o644)
 }
