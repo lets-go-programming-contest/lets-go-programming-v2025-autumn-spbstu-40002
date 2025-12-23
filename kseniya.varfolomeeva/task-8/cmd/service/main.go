1fb2a1a190c7af2590d50d44df66a303fe55d2d2
xanaoxx <znellaz25@gmail.com>
2025-12-23 20:54:38 +0300

    [TASK-8] Fix output format - remove newline

--- kseniya.varfolomeeva/task-8/cmd/service/main.go
+++ kseniya.varfolomeeva/task-8/cmd/service/main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"task-8/internal/config"
 )
 
 func main() {
 	cfg, err := config.Load()
 	if err != nil {
-		fmt.Printf("Failed to load config: %v\n", err)
-		return
+		fmt.Print("error")
+		os.Exit(1)
 	}
 	
-	fmt.Printf("%s %s\n", cfg.Environment, cfg.LogLevel)
+	fmt.Printf("%s %s", cfg.Environment, cfg.LogLevel)
 }
