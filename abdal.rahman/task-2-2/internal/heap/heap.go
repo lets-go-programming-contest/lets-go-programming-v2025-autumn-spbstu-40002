48bddbe6f983647a7f151ce07f6e391670dd6096
abdal.rahman <abdabublal1@gmail.com>
2026-01-08 16:42:32 +0200

    [TASK-2-2] Clean commit with only task-2-2 files

---
+++ abdal.rahman/task-2-2/internal/heap/heap.go
@@ -0,0 +1,20 @@
+﻿package heap
+
+type MaxHeap []int
+
+func (h MaxHeap) Len() int { return len(h) }
+func (h MaxHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h MaxHeap) Swap(i, j int) { h[i], h[j] = h[j], h[i] }
+
+func (h *MaxHeap) Push(val int) {
+*h = append(*h, val)
+}
+
+func (h *MaxHeap) Pop() int {
+if len(*h) == 0 {
+panic("heap is empty")
+}
+res := (*h)[0]
+*h = (*h)[1:]
+return res
+}
