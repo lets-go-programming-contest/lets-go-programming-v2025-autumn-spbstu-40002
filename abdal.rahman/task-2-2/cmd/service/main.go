23cd6b9663ebdc9fab169c874ba6bd645886f606
abdal.rahman <abdabublal1@gmail.com>
2026-01-08 18:47:18 +0200

    [TASK-2-2] Add heap implementation

---
+++ abdal.rahman/task-2-2/cmd/service/main.go
@@ -0,0 +1,41 @@
+﻿package main
+
+import (
+"fmt"
+"github.com/Xkoex/task-2-2/internal/heap"
+)
+
+func main() {
+var n, pref int
+fmt.Scan(&n)
+if n < 1 || n > 10000 {
+fmt.Println("invalid number of dishes")
+return
+}
+
+dishes := make([]int, n)
+for i := 0; i < n; i++ {
+fmt.Scan(&dishes[i])
+if dishes[i] < -10000 || dishes[i] > 10000 {
+fmt.Println("invalid rating")
+return
+}
+}
+
+fmt.Scan(&pref)
+if pref < 1 || pref > n {
+fmt.Println("invalid preference number")
+return
+}
+
+h := &heap.MaxHeap{}
+for _, d := range dishes {
+h.Push(d)
+}
+
+for i := 1; i < pref; i++ {
+h.Pop()
+}
+
+fmt.Println(h.Pop())
+}
