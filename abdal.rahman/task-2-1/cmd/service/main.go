eacc2e3753328df0af8c7d0388aaca40613130f5
abdal.rahman <abdabublal1@gmail.com>
2026-01-09 16:25:16 +0200

    [TASK-2-1] Code formatted and linted, ready for PR

--- abdal.rahman/task-2-1/cmd/service/main.go
+++ abdal.rahman/task-2-1/cmd/service/main.go
@@ -5,85 +5,92 @@ import (
 )
 
 const (
-	MinTempConst   = 15
-	MaxTempConst   = 30
-	MinCorrectData = 1
-	MaxCorrectData = 1000
+	minTemperatureConditioner = 15
+	maxTemperatureConditioner = 30
+	minDepartments            = 1
+	maxDepartments            = 1000
+	minEmployees              = 1
+	maxEmployees              = 1000
 )
 
-func isDataValid(data int) bool {
-	return data >= MinCorrectData && data <= MaxCorrectData
+type TemperatureRange struct {
+	minTemp int
+	maxTemp int
 }
 
-func adjustTemp(minTemp, maxTemp int, operator string, temp int) (int, int) {
+func NewTemperatureRange() *TemperatureRange {
+	return &TemperatureRange{
+		minTemp: minTemperatureConditioner,
+		maxTemp: maxTemperatureConditioner,
+	}
+}
+
+func (tr *TemperatureRange) Update(operator string, temperature int) bool {
+	if operator != ">=" && operator != "<=" {
+		return false
+	}
+
+	if temperature < minTemperatureConditioner || temperature > maxTemperatureConditioner {
+		return false
+	}
+
 	switch operator {
 	case ">=":
-		if temp > minTemp {
-			minTemp = temp
+		if temperature > tr.minTemp {
+			tr.minTemp = temperature
 		}
 	case "<=":
-		if temp < maxTemp {
-			maxTemp = temp
+		if temperature < tr.maxTemp {
+			tr.maxTemp = temperature
 		}
 	}
-	return minTemp, maxTemp
+
+	return true
+}
+
+func (tr *TemperatureRange) GetResult() int {
+	if tr.minTemp <= tr.maxTemp {
+		return tr.minTemp
+	}
+
+	return -1
 }
 
 func main() {
 	var departments int
 	_, err := fmt.Scan(&departments)
-	if err != nil {
+	if err != nil || departments < minDepartments || departments > maxDepartments {
 		return
 	}
 
-	if !isDataValid(departments) {
-		return
-	}
-
-	for range departments {
-		minTemp := MinTempConst
-		maxTemp := MaxTempConst
-		valid := true
-
-		var workers int
-		_, err = fmt.Scan(&workers)
-		if err != nil {
+	for i := 0; i < departments; i++ {
+		var employees int
+		_, err = fmt.Scan(&employees)
+		if err != nil || employees < minEmployees || employees > maxEmployees {
 			return
 		}
 
-		if !isDataValid(workers) {
-			return
-		}
+		tempRange := NewTemperatureRange()
 
-		for range workers {
+		for j := 0; j < employees; j++ {
 			var operator string
-			_, err = fmt.Scan(&operator)
-			if err != nil {
-				return
-			}
+			var temperature int
 
-			var temp int
-			_, err = fmt.Scan(&temp)
+			_, err = fmt.Scan(&operator, &temperature)
 			if err != nil {
-				return
-			}
+				fmt.Println(-1)
 
-			if temp < MinTempConst || temp > MaxTempConst {
-				valid = false
 				continue
 			}
 
-			minTemp, maxTemp = adjustTemp(minTemp, maxTemp, operator, temp)
+			ok := tempRange.Update(operator, temperature)
+			if !ok {
+				fmt.Println(-1)
 
-			if minTemp > maxTemp {
-				valid = false
+			} else {
+				fmt.Println(tempRange.GetResult())
+
 			}
-		}
-
-		if valid && minTemp <= maxTemp {
-			fmt.Println(minTemp)
-		} else {
-			fmt.Println(-1)
 		}
 	}
 }
