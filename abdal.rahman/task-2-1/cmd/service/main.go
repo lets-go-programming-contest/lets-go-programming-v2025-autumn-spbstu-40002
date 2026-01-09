237ee759a0eed5a5396c14d9adf9e077dd736c78
abdal.rahman <abdabublal1@gmail.com>
2026-01-09 16:44:21 +0200

    [TASK-2-1] Code formatted and linted, ready for PR

--- abdal.rahman/task-2-1/cmd/service/main.go
+++ abdal.rahman/task-2-1/cmd/service/main.go
@@ -13,8 +13,8 @@ const (
 )
 
 type TempData struct {
-	max int
 	min int
+	max int
 }
 
 var (
@@ -29,8 +29,8 @@ var (
 
 func newTempData(maxTemp, minTemp int) *TempData {
 	return &TempData{
-		max: maxTemp,
 		min: minTemp,
+		max: maxTemp,
 	}
 }
 
@@ -38,6 +38,7 @@ func (t *TempData) optimalTemp() int {
 	if t.min > t.max {
 		return -1
 	}
+
 	return t.min
 }
 
@@ -45,6 +46,7 @@ func (t *TempData) adjustTemp(operator string, temp int) error {
 	if temp < MinTemperature || temp > MaxTemperature {
 		return ErrTempOutOfRange
 	}
+
 	switch operator {
 	case ">=":
 		if temp > t.min {
@@ -57,15 +59,18 @@ func (t *TempData) adjustTemp(operator string, temp int) error {
 	default:
 		return ErrIncorrectSign
 	}
+
 	return nil
 }
 
 func main() {
 	var departments int
-	if _, err := fmt.Scan(&departments); err != nil {
+	_, err := fmt.Scan(&departments)
+	if err != nil {
 		fmt.Println(ErrIncorrectDepartments)
 		return
 	}
+
 	if departments < MinOfRange || departments > MaxOfRange {
 		fmt.Println(ErrDepOutOfRange)
 		return
@@ -73,10 +78,12 @@ func main() {
 
 	for range make([]struct{}, departments) {
 		var employees int
-		if _, err := fmt.Scan(&employees); err != nil {
+		_, err := fmt.Scan(&employees)
+		if err != nil {
 			fmt.Println(ErrIncorrectEmployees)
 			return
 		}
+
 		if employees < MinOfRange || employees > MaxOfRange {
 			fmt.Println(ErrEmpOutOfRange)
 			return
@@ -87,16 +94,20 @@ func main() {
 		for range make([]struct{}, employees) {
 			var operator string
 			var temp int
-			if _, err := fmt.Scan(&operator, &temp); err != nil {
+
+			_, err := fmt.Scan(&operator, &temp)
+			if err != nil {
 				fmt.Println(ErrIncorrectTemp)
 				return
 			}
 
-			if err := tempRange.adjustTemp(operator, temp); err != nil {
+			err = tempRange.adjustTemp(operator, temp)
+			if err != nil {
 				fmt.Println(-1)
-			} else {
-				fmt.Println(tempRange.optimalTemp())
+				continue
 			}
+
+			fmt.Println(tempRange.optimalTemp())
 		}
 	}
 }
